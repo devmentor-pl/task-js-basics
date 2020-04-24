ed401a982215c454eb0aaf5a028300903529944f
Dawid <dwilk92@o2.pl>
2020-04-24 23:44:47 +0200

    Ukonczony projekt

--- js/app.js
+++ js/app.js
@@ -8,110 +8,111 @@ Calculator.prototype.isCorrectAction = function(action) {
 }
 
 Calculator.prototype.getHistoryAsString = function() {
-    const arr = this.history;
-   
-    if (typeof action !== 'undefined')
-    {
-        arr.push(result)
-    }
     
     return this.history.join('\n');
 }
 
 Calculator.prototype.add = function(num1, num2) {
+Number.isNaN(num1)
+Number.isNaN(num2)
 
-    num1=parseInt(number1);
-    num2 = parseFloat(number2)
-    //console.log(typeof num1, typeof num2 )
-    if(typeof num1 === 'number' && typeof num2 === 'number')
+
+    if(Number.isNaN(num1) === false && Number.isNaN(num2) === false)
    {
-        const addResult = num1 + num2
-        result = num1 + ' + ' + num2 + ' = ' + addResult;
-        //console.log(result);
-        return result;  
-    }
+    let a = parseInt(num1)
+    let b = parseInt(num2)
+        const addResult = a + b
+        result = a+ ' + ' + b + ' = ' + addResult;
+        //console.log(result); 
+        this.history.push(result)  
+   }
     else
-    console.log('podane dane nie są liczbą')
+   alert('podane dane nie są liczbą')
 }
     // 1. zamień wartości przekazane przez parametr na typ number
     // 2. sprawdź czy są one poprawne
     // 3. jeśli tak to wykonaj działanie i zapisz jego resultat
     // 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
     Calculator.prototype.diff = function(num1, num2) {
-        num1=parseInt(number1);
-    num2 = parseFloat(number2)
-    //console.log(typeof num1, typeof num2 )
-    if(typeof num1 === 'number' && typeof num2 === 'number')
-   {
-        const diffResult = num1 - num2
-        result = num1 + ' - ' + num2 + ' = ' + diffResult;
-        //console.log(result);
-        return result;  
-    }
+        Number.isNaN(num1)
+        Number.isNaN(num2)
+        
+            if(Number.isNaN(num1) === false && Number.isNaN(num2) === false)
+           {
+            let a = parseInt(num1)
+            let b = parseInt(num2)
+                const addResult = a - b
+                result = a+ ' - ' + b + ' = ' + addResult;
+                //console.log(result);
+                this.history.push(result)  
+           }
     else
-    console.log('podane dane nie są liczbą')
+    alert('podane dane nie są liczbą')
 }
 
     Calculator.prototype.mult = function(num1, num2) {
-        num1=parseInt(number1);
-        num2 = parseFloat(number2)
-        // console.log(typeof num1, typeof num2 )
-        if(typeof num1 === 'number' && typeof num2 === 'number')
+        Number.isNaN(num1)
+        Number.isNaN(num2)
+
+        if(Number.isNaN(num1) === false && Number.isNaN(num2) === false)
         {
-            const multResult = num1 * num2
-            result = num1 + ' * ' + num2 + ' = ' + multResult;
-           // console.log(result);
-            return result;  
+        let a = parseInt(num1)
+        let b = parseInt(num2)
+        const addResult = a * b
+        result = a+ ' * ' + b + ' = ' + addResult;
+        //console.log(result);
+        this.history.push(result)  
         }
         else
-        console.log('podane dane nie są liczbą')
+        alert('podane dane nie są liczbą')
 }
 
 Calculator.prototype.div = function(num1, num2) {
-    num1=parseInt(number1);
-    num2 = parseFloat(number2)
-    //console.log(typeof num1, typeof num2 )
-    if(typeof num1 === 'number' && typeof num2 === 'number')
-    {
-        const divResult = num1 / num2
-        result = num1 + ' / ' + num2 + ' = ' + divResult;
-       // console.log(result);
-        return result;  
-    }
+    Number.isNaN(num1)
+    Number.isNaN(num2)
+
+    if(Number.isNaN(num1) === false && Number.isNaN(num2) === false)
+   {
+    let a = parseInt(num1)
+    let b = parseInt(num2)
+        const addResult = a / b
+        result = a+ ' / ' + b + ' = ' + addResult;
+        //console.log(result);
+        this.history.push(result)  
+   }
     else
-    console.log('podane dane nie są liczbą')
+    alert('podane dane nie są liczbą')
 }
 
 Calculator.prototype.pow = function(num1, num2) {
-    num1=parseInt(number1);
-    num2 = parseFloat(number2)
-    console.log(typeof num1, typeof num2 )
-    if(typeof num1 === 'number' && typeof num2 === 'number')
+    Number.isNaN(num1)
+    Number.isNaN(num2)
+    
+    if(Number.isNaN(num1) === false && Number.isNaN(num2) === false)
         {   
+            let a = parseInt(num1)
+            let b = parseInt(num2)
             let i = 0;
             let str= ''
-            result = num1 ** num2 
-            while (i < num2)
+            result = a ** b 
+            while (i < b)
             {
                 if (i === 0)
                 {
-                    str = num1;
+                    str = a;
                 }
                 else
                 {
-                str = str + ' * ' + num1
+                str = str + ' * ' + a
                 }
                 i++;
             }
-           // const result = str + ' = ' + result <- próbowałem wersję const powResult = str + '=' + result a potem return powResult
-           // w .getHistoryAsString robiłem specjalny warunek tylko dla potęg, ale wtedy mi wywalało informację, że nie umie znaleźć powResult...
-            //console.log(result) 
-            return result; // w przeglądarce w konsoli pokazuje mi cały wynik czyli num1*num1*num1 ale zapisuje mi 
-            // jedynie wynik końcowy... a nie całe działanie robiłem różne kombinacje 
+          
+            this.history.push(str + ' = ' + result) 
 
     }
     else
-    console.log('podane dane nie są liczbą')
+    alert('podane dane nie są liczbą')
 }
 
 const calc = new Calculator();
