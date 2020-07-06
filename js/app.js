9d46f884203b24d649b5725b78d550bce28b76f7
jakubsmiarowski <48868566+jakubsmiarowski@users.noreply.github.com>
2020-07-06 19:42:15 +0200

    task done

--- js/app.js
+++ js/app.js
@@ -12,28 +12,74 @@ Calculator.prototype.getHistoryAsString = function() {
 }
 
 Calculator.prototype.add = function(num1, num2) {
+    const number1 = parseInt(num1);
+    const number2 = parseInt(num2);
+    if (typeof number1 === 'number' && typeof number2 === 'number') {
+        const result = number1 + number2;
+        console.log(`Wynik działania ${number1} + ${number2} to ${result}`);
+        this.history.push(`${number1} + ${number2} = ${result}`)
+    }
     // 1. zamień wartości przekazane przez parametr na typ number
     // 2. sprawdź czy są one poprawne
     // 3. jeśli tak to wykonaj działanie i zapisz jego resultat
     // 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
 }
 
+Calculator.prototype.substract = function(num1, num2) {
+    const number1 = parseInt(num1);
+    const number2 = parseInt(num2);
+    if (typeof number1 === 'number' && typeof number2 === 'number') {
+        const result = number1 - number2;
+        console.log(`Wynik działania ${number1} - ${number2} to ${result}`);
+        this.history.push(`${number1} - ${number2} = ${result}`)
+    }
+}
+
+Calculator.prototype.multiply = function(num1, num2) {
+    const number1 = parseInt(num1);
+    const number2 = parseInt(num2);
+    if (typeof number1 === 'number' && typeof number2 === 'number') {
+        const result = number1 * number2;
+        console.log(`Wynik działania ${number1} * ${number2} to ${result}`);
+        this.history.push(`${number1} * ${number2} = ${result}`)
+    }
+}
+
+Calculator.prototype.divide = function(num1, num2) {
+    const number1 = parseInt(num1);
+    const number2 = parseInt(num2);
+    if (typeof number1 === 'number' && typeof number2 === 'number') {
+        const result = number1 / number2;
+        console.log(`Wynik działania ${number1} / ${number2} to ${result}`);
+        this.history.push(`${number1} / ${number2} = ${result}`)
+    }
+}
+
 const calc = new Calculator();
+const prototype = Object.getPrototypeOf(calc);
+console.log(prototype);
 let action, promptContent, isCorrectAction, number1, number2;
-do { 
+do {
     promptContent = 'Podaj jaką operację chcesz wykonać (+, -, *, /, ^) i potwierdź. \n'; // \n - znak nowej linii
     promptContent += 'Jeśli chcesz zrezygnować wciśnij Anuluj. \n';
     promptContent += 'Lista poprzednich operacji: \n' + calc.getHistoryAsString();
 
     action = prompt(promptContent);
     isCorrectAction = calc.isCorrectAction(action);
-    if(isCorrectAction) {
+    if (isCorrectAction) {
         number1 = prompt('Podaj liczbę nr 1');
         number2 = prompt('Podaj liczbę nr 2');
 
-        if(action === '+') {
+        if (action === '+') {
             calc.add(number1, number2);
+        } else if (action === '-') {
+            calc.substract(number1, number2);
+        } else if (action === '*') {
+            calc.multiply(number1, number2);
+        } else if (action === '/') {
+            calc.divide(number1, number2)
         }
+
     }
-    
-} while(calc.isCorrectAction(action));
+
+} while (calc.isCorrectAction(action));
