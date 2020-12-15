242835713be0a0e916525a9c0c026654482c16c1
Dawid <kdpietraszko@wp.pl>
2020-12-15 21:00:56 +0100

    task update 2

--- js/app.js
+++ js/app.js
@@ -11,6 +11,22 @@ Calculator.prototype.getHistoryAsString = function () {
     return this.history.join('\n');
 }
 
+Calculator.prototype.areCorrectNumbers = function (checkNum1, checkNum2) {
+    if (isNaN(checkNum1) || isNaN(checkNum2)) {
+        alert('Co najmniej jedna z podanych danych nie jest liczbą !!!');
+
+        return true;
+    }
+
+    return false;
+}
+
+Calculator.prototype.arrPushResult = function (num1, num2, result, actions) {
+    return this.history.push(num1 + ' ' + actions + ' ' + num2 + ' = ' + result);
+}
+
+
+
 Calculator.prototype.add = function (num1, num2) {
     // 1. zamień wartości przekazane przez parametr na typ number
     // 2. sprawdź czy są one poprawne
@@ -19,11 +35,9 @@ Calculator.prototype.add = function (num1, num2) {
     const numberAdd1 = parseFloat(num1);
     const numberAdd2 = parseFloat(num2);
 
-    const checkNumAdd = checkNumbers(numberAdd1, numberAdd2);
-
-    if (checkNumAdd === false) {
+    if (calc.areCorrectNumbers(numberAdd1, numberAdd2) === false) {
         const resultAdd = numberAdd1 + numberAdd2;
-        arrPushResult(this.history, numberAdd1, numberAdd2, resultAdd, action);
+        calc.arrPushResult(numberAdd1, numberAdd2, resultAdd, action);
     }
 }
 
@@ -32,11 +46,9 @@ Calculator.prototype.sub = function (num1, num2) {
     const numberSub1 = parseFloat(num1);
     const numberSub2 = parseFloat(num2);
 
-    const checkNumSub = checkNumbers(numberSub1, numberSub2);
-
-    if (checkNumSub === false) {
+    if (calc.areCorrectNumbers(numberSub1, numberSub2) === false) {
         const resultSub = numberSub1 - numberSub2;
-        arrPushResult(this.history, numberSub1, numberSub2, resultSub, action);
+        calc.arrPushResult(numberSub1, numberSub2, resultSub, action);
     }
 }
 
@@ -45,11 +57,9 @@ Calculator.prototype.mul = function (num1, num2) {
     const numberMul1 = parseFloat(num1);
     const numberMul2 = parseFloat(num2);
 
-    const checkNumMul = checkNumbers(numberMul1, numberMul2);
-
-    if (checkNumMul === false) {
+    if (calc.areCorrectNumbers(numberMul1, numberMul2) === false) {
         const resultMul = numberMul1 * numberMul2;
-        arrPushResult(this.history, numberMul1, numberMul2, resultMul, action);
+        calc.arrPushResult(numberMul1, numberMul2, resultMul, action);
     }
 }
 
@@ -58,11 +68,9 @@ Calculator.prototype.div = function (num1, num2) {
     const numberDiv1 = parseFloat(num1);
     const numberDiv2 = parseFloat(num2);
 
-    const checkNumDiv = checkNumbers(numberDiv1, numberDiv2);
-
-    if (checkNumDiv === false) {
+    if (calc.areCorrectNumbers(numberDiv1, numberDiv2) === false) {
         const resultDiv = numberDiv1 / numberDiv2;
-        arrPushResult(this.history, numberDiv1, numberDiv2, resultDiv, action);
+        calc.arrPushResult(numberDiv1, numberDiv2, resultDiv, action);
     }
 }
 
@@ -71,18 +79,17 @@ Calculator.prototype.pow = function (num1, num2) {
     const numberPow1 = parseFloat(num1);
     const numberPow2 = parseFloat(num2);
 
-    const checkNumPow = checkNumbers(numberPow1, numberPow2);
-
-    if (checkNumPow === false) {
+    if (calc.areCorrectNumbers(numberPow1, numberPow2) === false) {
         if (numberPow2 > 0) {
             let resultPow = 1;
             for (let i = 0; i < numberPow2; i++) {
                 resultPow *= numberPow1;
             }
-            arrPushResult(this.history, numberPow1, numberPow2, resultPow, action);
+            calc.arrPushResult(numberPow1, numberPow2, resultPow, action);
+
         } else if (numberPow2 === 0) {
             let resultPow = 1;
-            arrPushResult(this.history, numberPow1, numberPow2, resultPow, action);
+            calc.arrPushResult(numberPow1, numberPow2, resultPow, action);
         } else {
             let resultPow = 0;
             let resultPowNegative = 1;
@@ -92,7 +99,7 @@ Calculator.prototype.pow = function (num1, num2) {
                 resultPowNegative *= numberPow1;
                 resultPow = 1 / resultPowNegative;
             }
-            arrPushResult(this.history, numberPow1, numberPow2, resultPow, action);
+            calc.arrPushResult(numberPow1, numberPow2, resultPow, action);
         }
     }
 }
@@ -125,20 +132,3 @@ do {
     }
 
 } while (calc.isCorrectAction(action));
-
-
-function checkNumbers(checkNum1, checkNum2) {
-
-    if (isNaN(checkNum1) || isNaN(checkNum2)) {
-        alert('Co najmniej jedna z podanych danych nie jest liczbą !!!');
-
-        return true;
-    }
-
-    return false;
-}
-
-
-function arrPushResult(arr, num1, num2, result, actions) {
-    return arr.push(num1 + ' ' + actions + ' ' + num2 + ' = ' + result);
-}
