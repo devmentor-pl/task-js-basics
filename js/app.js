446aecd3710568a450b40393c41a9a812d7c77c7
Dominik Mozdzen <dominik.mozdzen1@gmail.com>
2021-04-22 19:16:06 +0200

    Project

--- js/app.js
+++ js/app.js
@@ -11,11 +11,98 @@ Calculator.prototype.getHistoryAsString = function() {
     return this.history.join('\n');
 }
 
+Calculator.prototype.isNumber = function (number) {
+    if(typeof number === "number" && !isNaN(number)) {
+        return true;
+    } else {
+        return alert("Podano nieprawidłowe wartości");
+    }
+}
+
 Calculator.prototype.add = function(num1, num2) {
-    // 1. zamień wartości przekazane przez parametr na typ number
-    // 2. sprawdź czy są one poprawne
-    // 3. jeśli tak to wykonaj działanie i zapisz jego resultat
-    // 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
+
+    let a = num1;
+    let b = num2;
+    let result = 0;
+
+    while(this.isNumber(num1) && this.isNumber(num2)) {
+
+        result = a + b;
+        this.history.push(a  + " + " + b + " = " + result);
+        
+        return result;
+    
+    }
+}
+
+Calculator.prototype.subtraction = function(num1, num2) {
+
+    let a = num1;
+    let b = num2;
+    let result = 0;
+
+    while(this.isNumber(num1) && this.isNumber(num2)) {
+
+        result = a - b;
+        this.history.push(a  + " - " + b + " = " + result);
+        
+        return result;
+    
+    }
+}
+
+Calculator.prototype.multiplication = function(num1, num2) {
+
+    let a = num1;
+    let b = num2;
+    let result = 0;
+
+    while(this.isNumber(num1) && this.isNumber(num2)) {
+
+        result = a * b;
+        this.history.push(a  + " * " + b + " = " + result);
+        
+        return result;
+    
+    }
+}
+
+Calculator.prototype.divide = function(num1, num2) {
+
+    let a = num1;
+    let b = num2;
+    let result = 0;
+
+    while(this.isNumber(num1) && this.isNumber(num2) && num2 !== 0) {
+
+        result = a / b;
+        this.history.push(a  + " / " + b + " = " + result);
+        
+        return result;
+    
+    }
+}
+
+Calculator.prototype.exponentiation = function(num1, num2) {
+
+    let a = num1;
+    let b = num2;
+    let result = 1;
+
+    while(this.isNumber(num1) && this.isNumber(num2) && Math.round(num2) === num2) {
+
+        if(b <= 0) {
+            result = 1;
+        } else {
+            for(let i = 1; i <= b; i++) {
+                result *= a;
+            }
+        }
+        
+        this.history.push(a  + " ^ " + b + " = " + result);
+        return result;
+    
+    }
 }
 
 const calc = new Calculator();
@@ -28,12 +115,21 @@ do {
     action = prompt(promptContent);
     isCorrectAction = calc.isCorrectAction(action);
     if(isCorrectAction) {
-        number1 = prompt('Podaj liczbę nr 1');
-        number2 = prompt('Podaj liczbę nr 2');
+        number1 = parseFloat(prompt('Podaj liczbę nr 1'));
+        number2 = parseFloat(prompt('Podaj liczbę nr 2'));
 
         if(action === '+') {
             calc.add(number1, number2);
+        } else if(action === "-") {
+            calc.subtraction(number1, number2)
+        } else if(action === "*") {
+            calc.multiplication(number1, number2)
+        } else if(action === "/") {
+            calc.divide(number1, number2);
+        } else if(action === "^") {
+            calc.exponentiation(number1, number2)
         }
     }
     
 } while(calc.isCorrectAction(action));
+
