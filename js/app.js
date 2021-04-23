a2c5c402711385823f6347f9b3db3df0e2c19729
Radosław Kawczyński <kawczynski.radoslaw@gmail.com>
2021-04-23 15:27:34 +0200

    task-js-basis: calculator
    task-js-basis: calculator

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 function Calculator() {
     this.actions = ['+', '-', '*', '/', '^'];
     this.history = [];
@@ -11,15 +12,81 @@ Calculator.prototype.getHistoryAsString = function() {
     return this.history.join('\n');
 }
 
+Calculator.prototype.isNumber = function (number) {
+
+    if (typeof parseFloat(number) === 'number') {  //sprawdź czy number
+        return true;
+    } else {
+        return alert('Nieprawidłowa wartość!');
+    }
+
+}
+
+// 1. zamień wartości przekazane przez parametr na typ number
+// 2. sprawdź czy są one poprawne
+// 3. jeśli tak to wykonaj działanie i zapisz jego resultat
+// 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
+
+// dodawanie (+)
 Calculator.prototype.add = function(num1, num2) {
-    // 1. zamień wartości przekazane przez parametr na typ number
-    // 2. sprawdź czy są one poprawne
-    // 3. jeśli tak to wykonaj działanie i zapisz jego resultat
-    // 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
+    let result = 0;
+    do {
+        result = parseFloat(num1) + parseFloat(num2);
+        this.history.push(`${number1} + ${number2} = ${result}`);
+        return result;
+    } while (this.isNumber(num1) && this.isNumber(num2))
+}
+
+// odejmowanie (-)
+Calculator.prototype.substraction = function(num1, num2) {
+    let result = 0;
+    do {
+        result = parseFloat(num1) - parseFloat(num2);
+        this.history.push(`${number1} - ${number2} = ${result}`);
+        return result;
+    } while (this.isNumber(num1) && this.isNumber(num2))
+}
+
+// mnożenie (*)
+Calculator.prototype.multiplication = function(num1, num2) {
+    let result = 0;
+    do {
+        result = parseFloat(num1) * parseFloat(num2);
+        this.history.push(`${number1} * ${number2} = ${result}`);
+        return result;
+    } while (this.isNumber(num1) && this.isNumber(num2))
+}
+
+// dzielenie (/)
+Calculator.prototype.divide = function(num1, num2) {
+    let result = 0;
+    do {
+        result = parseFloat(num1) / parseFloat(num2);
+        this.history.push(`${number1} / ${number2} = ${Math.round(result * 1000) / 1000}`);
+        return Math.round(result * 1000) / 1000;
+    } while (this.isNumber(num1) && this.isNumber(num2))
+}
+
+// potęgowanie (^) - wykonaj to rozwiązanie przy pomocy pętli, nie korzystaj z *Math.pow()*;
+Calculator.prototype.exponentiation = function(num1, num2) {
+    let result = 1;
+    do {
+        if (num2 <= 0) {
+            result = 1;
+        } else {
+            for (let i = 1; i <= num2; i++) {
+                result = result * num1;
+            }
+        }
+        this.history.push(`${number1} ^ ${number2} = ${result}`);
+        return result;
+    } while (this.isNumber(num1) && this.isNumber(num2))
 }
 
 const calc = new Calculator();
+
 let action, promptContent, isCorrectAction, number1, number2;
+
 do { 
     promptContent = 'Podaj jaką operację chcesz wykonać (+, -, *, /, ^) i potwierdź. \n'; // \n - znak nowej linii
     promptContent += 'Jeśli chcesz zrezygnować wciśnij Anuluj. \n';
@@ -27,12 +94,29 @@ do {
 
     action = prompt(promptContent);
     isCorrectAction = calc.isCorrectAction(action);
-    if(isCorrectAction) {
+
+    if (isCorrectAction) {
         number1 = prompt('Podaj liczbę nr 1');
         number2 = prompt('Podaj liczbę nr 2');
 
-        if(action === '+') {
+        if (action === '+') {
             calc.add(number1, number2);
+        } else {
+            if (action === '-') {
+                calc.substraction(number1, number2);
+            } else {
+                if (action === '*') {
+                    calc.multiplication(number1, number2);
+                } else {
+                    if (action === '/') {
+                        calc.divide(number1, number2);
+                    } else {
+                        if (action === '^') {
+                            calc.exponentiation(number1, number2);
+                        }
+                    }
+                }
+            }
         }
     }
     
