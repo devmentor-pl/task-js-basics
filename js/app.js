e83f1eeccf24159caab8f21e7e976519f982cb84
Malgorzata Kowacka <kowackag@gmail.com>
2021-06-10 09:38:38 +0200

    Change function isCorrectValues on isCorrectValue
    To check every number separatelly

--- js/app.js
+++ js/app.js
@@ -3,119 +3,14 @@ function Calculator() {
     this.history = [];
 }
 
-function isCorrectValue(num1, num2) {
-    return (Number.isNaN(num1)) || Number.isNaN(num2) ? false : true;
-}
-
-Calculator.prototype.isCorrectAction = function (action) {
-    return this.actions.includes(action);
-}
-
-Calculator.prototype.getHistoryAsString = function () {
-    return this.history.join('\n');
-}
-
-Calculator.prototype.add = function (num1, num2) {
-    if (isCorrectValue(num1, num2)) {
-        this.history.push(`${num1} ${action} ${num2} = ${num1+num2}`);
-    } else {
-        this.history.push('Podano błędne liczby');
-    }
-}
-
-Calculator.prototype.subtract = function (num1, num2) {
-    if (isCorrectValue(num1, num2)) {
-        this.history.push(`${num1} ${action} ${num2} = ${num1-num2}`);
-    } else {
-        this.history.push('Podano błędne liczby');
-    }
-}
-
-Calculator.prototype.multiply = function (num1, num2) {
-    if (isCorrectValue(num1, num2)) {
-        this.history.push(`${num1} ${action} ${num2} = ${num1*num2}`);
-    } else {
-        this.history.push('Podano błędne liczby');
-    }
-}
-
-Calculator.prototype.divide = function (num1, num2) {
-    if (isCorrectValue(num1, num2)) {
-        if (num2 === 0) {
-            this.history.push(`Próba dzielenia przez 0`)
-        } else {
-            this.history.push(`${num1} ${action} ${num2} = ${num1/num2}`);
-        }
-    } else {
-        this.history.push('Podano błędne liczby');
-    }
-}
-
-Calculator.prototype.exponentiate = function (num1, num2) {
-    if (isCorrectValue(num1, num2)) {
-        if (num2 >= 0) {
-            let exponentResult = 1;
-            for (let i = 0; i < num2; i++) {
-                exponentResult *= num1;
-            }
-            this.history.push(`${num1} ${action} ${num2} = ${exponentResult}`);
-        } else {
-            let exponentResult = 1;
-            for (let i = 0; i > num2; i--) {
-                exponentResult *= 1 / num1;
-            }
-            this.history.push(`${num1} ${action} ${num2} = ${exponentResult}`);
-        }
-    } else {
-        this.history.push('Podano błędne liczby');
-    }
-}
-
-const calc = new Calculator();
-let action, promptContent, isCorrectAction, number1, number2;
-do {
-    promptContent = 'Podaj jaką operację chcesz wykonać (+, -, *, /, ^) i potwierdź. \n'; // \n - znak nowej linii
-    promptContent += 'Jeśli chcesz zrezygnować wciśnij Anuluj. \n';
-    promptContent += 'Lista poprzednich operacji: \n' + calc.getHistoryAsString();
-
-    action = prompt(promptContent);
-    isCorrectAction = calc.isCorrectAction(action);
-    if (isCorrectAction) {
-        number1 = Number(prompt('Podaj liczbę nr 1'));
-        number2 = Number(prompt('Podaj liczbę nr 2'));
-
-        if (action === '+') {
-            calc.add(number1, number2);
-        } else if (action === '-') {
-            calc.subtract(number1, number2);
-        } else if (action === '*') {
-            calc.multiply(number1, number2);
-        } else if (action === '/') {
-            calc.divide(number1, number2);
-        } else {
-            calc.exponentiate(number1, number2)
-        }
-    }
-
-} while (calc.isCorrectAction(action));function Calculator() {
-    this.actions = ['+', '-', '*', '/', '^'];
-    this.history = [];
-}
-
-function isCorrectValues(num1, num2) {
-    return (Number.isNaN(num1)) || Number.isNaN(num2) ? false : true;
+Calculator.prototype.isCorrectValue = function (num) {
+    return Number.isNaN(num) ? false : true;
 }
 
 Calculator.prototype.addToHistory = function (num1, num2, action, result) {
     this.history.push(`${num1} ${action} ${num2} = ${result}`)
 }
 
-// ------DLACZEGO NIE TAK ------
-
-// function addToHistory(num1, num2, action, result) {
-//     calc.history.push(`${num1} ${action} ${num2} = ${result}`) 
-// }
-
 Calculator.prototype.isCorrectAction = function (action) {
     return this.actions.includes(action);
 }
@@ -125,7 +20,7 @@ Calculator.prototype.getHistoryAsString = function () {
 }
 
 Calculator.prototype.add = function (num1, num2) {
-    if (isCorrectValue(num1, num2)) {
+    if (this.isCorrectValue(num1) && this.isCorrectValue(num2)) {
         result = num1 + num2;
     } else {
         result = 'Podano błędne liczby';
@@ -133,7 +28,7 @@ Calculator.prototype.add = function (num1, num2) {
 }
 
 Calculator.prototype.subtract = function (num1, num2) {
-    if (isCorrectValue(num1, num2)) {
+    if (this.isCorrectValue(num1) && this.isCorrectValue(num2)) {
         result = num1 - num2;
     } else {
         result = 'Podano błędne liczby';
@@ -141,7 +36,7 @@ Calculator.prototype.subtract = function (num1, num2) {
 }
 
 Calculator.prototype.multiply = function (num1, num2) {
-    if (isCorrectValue(num1, num2)) {
+    if (this.isCorrectValue(num1) && this.isCorrectValue(num2)) {
         result = num1 * num2;
     } else {
         result = 'Podano błędne liczby';
@@ -149,7 +44,7 @@ Calculator.prototype.multiply = function (num1, num2) {
 }
 
 Calculator.prototype.divide = function (num1, num2) {
-    if (isCorrectValue(num1, num2)) {
+    if (this.isCorrectValue(num1) && this.isCorrectValue(num2)) {
         if (num2 === 0) {
             result = "Próba dzielenia przez 0";
         } else {
@@ -161,7 +56,7 @@ Calculator.prototype.divide = function (num1, num2) {
 }
 
 Calculator.prototype.exponentiate = function (num1, num2) {
-    if (isCorrectValue(num1, num2)) {
+    if (this.isCorrectValue(num1) && this.isCorrectValue(num2)) {
         if (num2 >= 0) {
             let exponentResult = 1;
             for (let i = 0; i < num2; i++) {
@@ -205,7 +100,6 @@ do {
             calc.exponentiate(number1, number2)
         }
         calc.addToHistory(number1, number2, action, result);
-        // addToHistory(number1, number2, action, result);
     }
 
 } while (calc.isCorrectAction(action));
