b960a59d3e5758c01b77428f40cd6137dfa1838f
jchrobakk <jchrobakk@gmail.com>
2021-08-10 16:52:17 +0200

    Code add function

--- js/app.js
+++ js/app.js
@@ -13,9 +13,13 @@ Calculator.prototype.getHistoryAsString = function() {
 
 Calculator.prototype.add = function(num1, num2) {
     // 1. zamień wartości przekazane przez parametr na typ number
+    num1= Number(num1)
+    num2 = Number(num2)
     // 2. sprawdź czy są one poprawne
     // 3. jeśli tak to wykonaj działanie i zapisz jego resultat
+    result = num1+num2
     // 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
+    this.history.push(`${num1} + ${num2} = ${result}`)
 }
 
 const calc = new Calculator();
