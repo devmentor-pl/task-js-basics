a0317efa614660e63f05217d3127c4675dafcade
Rafał Kazik <rafalkazik@MacBook-Air-Rafal.local>
2021-11-10 20:47:07 +0100

    task amendment

--- js/app.js
+++ js/app.js
@@ -13,67 +13,42 @@ Calculator.prototype.getHistoryAsString = function () {
 
 // ADDIND
 Calculator.prototype.add = function (num1, num2) {
-  if (isNaN(num1) || isNaN(num2)) {
-    alert("Podałeś błędne wartości! Podaj cyfry z zakresu od 0 do 9! ");
-  } else {
-    let sum = 0;
-    let sumStringResult = "";
-    sum = num1 + num2;
-    sumStringResult = `${num1} + ${num2} = ${sum}`;
-    this.history.push(sumStringResult);
-  }
+  let sum = 0;
+  sum = num1 + num2;
+  let sumStringResult = `${num1} + ${num2} = ${sum}`;
+  this.history.push(sumStringResult);
 };
 
 // SUBTRACTION
 Calculator.prototype.subtract = function (num1, num2) {
-  if (isNaN(num1) || isNaN(num2)) {
-    alert("Podałeś błędne wartości! Podaj cyfry z zakresu od 0 do 9! ");
-  } else {
-    let subt = 0;
-    let subtStringResult = "";
-    subt = num1 - num2;
-    subtStringResult = `${num1} - ${num2} = ${subt}`;
-    this.history.push(subtStringResult);
-  }
+  let subt = 0;
+  subt = num1 - num2;
+  let subtStringResult = `${num1} - ${num2} = ${subt}`;
+  this.history.push(subtStringResult);
 };
 
 // MULTIPLICATION
 Calculator.prototype.multiply = function (num1, num2) {
-  if (isNaN(num1) || isNaN(num2)) {
-    alert("Podałeś błędne wartości! Podaj cyfry z zakresu od 0 do 9! ");
-  } else {
-    let mult = 0;
-    let multStringResult = "";
-    mult = num1 * num2;
-    multStringResult = `${num1} * ${num2} = ${mult}`;
-    this.history.push(multStringResult);
-  }
+  let mult = 0;
+  mult = num1 * num2;
+  let multStringResult = `${num1} * ${num2} = ${mult}`;
+  this.history.push(multStringResult);
 };
 
 // DIVISION
 Calculator.prototype.divide = function (num1, num2) {
-  if (isNaN(num1) || isNaN(num2)) {
-    alert("Podałeś błędne wartości! Podaj cyfry z zakresu od 0 do 9! ");
-  } else {
-    let div = 0;
-    let divStringResult = "";
-    div = num1 / num2;
-    divStringResult = `${num1} / ${num2} = ${div}`;
-    this.history.push(divStringResult);
-  }
+  let div = 0;
+  div = num1 / num2;
+  let divStringResult = `${num1} / ${num2} = ${div}`;
+  this.history.push(divStringResult);
 };
 
 // EXPONENTATION
 Calculator.prototype.exponetiate = function (num1, num2) {
-  if (isNaN(num1) || isNaN(num2)) {
-    alert("Podałeś błędne wartości! Podaj cyfry z zakresu od 0 do 9! ");
-  } else {
-    let exp = 0;
-    let expStringResult = "";
-    exp = num1 ** num2;
-    expStringResult = `${num1} ^ ${num2} = ${exp}`;
-    this.history.push(expStringResult);
-  }
+  let exp = 0;
+  exp = num1 ** num2;
+  let expStringResult = `${num1} ^ ${num2} = ${exp}`;
+  this.history.push(expStringResult);
 };
 
 //
@@ -92,20 +67,17 @@ do {
   if (isCorrectAction) {
     number1 = parseInt(prompt("Podaj liczbę nr 1"));
     number2 = parseInt(prompt("Podaj liczbę nr 2"));
-
-    if (action === "+") {
+    if (isNaN(number1) || isNaN(number2)) {
+      alert("Podałeś błędne wartości! Podaj cyfry z zakresu od 0 do 9! ");
+    } else if (action === "+") {
       calc.add(number1, number2);
-    }
-    if (action === "-") {
+    } else if (action === "-") {
       calc.subtract(number1, number2);
-    }
-    if (action === "*") {
+    } else if (action === "*") {
       calc.multiply(number1, number2);
-    }
-    if (action === "/") {
+    } else if (action === "/") {
       calc.divide(number1, number2);
-    }
-    if (action === "^") {
+    } else if (action === "^") {
       calc.exponetiate(number1, number2);
     }
   }
