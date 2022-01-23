3b8b38321f1d82900aa55ceef31c8c6b397841ff
Max <maciejdobisz@gmail.com>
2022-01-23 22:55:44 +0000

    task js baiscs

--- js/app.js
+++ js/app.js
@@ -12,28 +12,83 @@ Calculator.prototype.getHistoryAsString = function() {
 }
 
 Calculator.prototype.add = function(num1, num2) {
-    // 1. zamień wartości przekazane przez parametr na typ number
-    // 2. sprawdź czy są one poprawne
-    // 3. jeśli tak to wykonaj działanie i zapisz jego resultat
-    // 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
+    const x=parseInt(num1);
+    const y=parseInt(num2);
+    if(typeof x==='number'&&typeof y==='number') {
+        const result = x+y;
+        this.history.push(x+'+'+y+'='+result);
+    }
+}
+
+Calculator.prototype.subtract = function(num1, num2) {
+    const x=parseInt(num1);
+    const y=parseInt(num2);
+    if(typeof x==='number'&&typeof y==='number') {
+        const result = x - y;
+        this.history.push(x+'-'+y+'='+result);
+    }
+}
+
+Calculator.prototype.multiply = function(num1, num2) {
+    const x=parseInt(num1);
+    const y=parseInt(num2);
+    if(typeof x==='number'&&typeof y==='number') {
+        const result = x * y;
+        this.history.push(x+'*'+y+'='+result);
+    }
+}
+
+Calculator.prototype.divide = function(num1, num2) {
+    const x=parseInt(num1);
+    const y=parseInt(num2);
+    if(typeof x==='number'&&typeof y==='number') {
+        const result = x / y;
+        this.history.push(x+'/'+y+'='+result);
+    }
+}
+
+Calculator.prototype.power = function(num1, num2) {
+    const x=parseInt(num1);
+    const y=parseInt(num2);
+    if(typeof x==='number'&&typeof y==='number') {
+        let result = x;
+        if(y!==0) {for(let i=2; i<=y; i++) {result*=x;}this.history.push(x+'^'+y+'='+result);} else {
+            this.history.push(x+'^'+y+'='+1);
+        }
+    }
 }
 
 const calc = new Calculator();
 let action, promptContent, isCorrectAction, number1, number2;
-do { 
-    promptContent = 'Podaj jaką operację chcesz wykonać (+, -, *, /, ^) i potwierdź. \n'; // \n - znak nowej linii
-    promptContent += 'Jeśli chcesz zrezygnować wciśnij Anuluj. \n';
-    promptContent += 'Lista poprzednich operacji: \n' + calc.getHistoryAsString();
 
+do { 
+    promptContent = 'Podaj jaką operację chcesz wykonać (+, -, *, /, ^) i potwierdź. \nJeśli chcesz zrezygnować wciśnij Anuluj.\nLista poprzednich operacji: \n' + calc.getHistoryAsString();
     action = prompt(promptContent);
     isCorrectAction = calc.isCorrectAction(action);
     if(isCorrectAction) {
         number1 = prompt('Podaj liczbę nr 1');
         number2 = prompt('Podaj liczbę nr 2');
-
-        if(action === '+') {
+        switch(action) {
+            case '+':
             calc.add(number1, number2);
+            break;
+            case '-':
+            calc.subtract(number1, number2);
+            break;
+            case '*':
+            calc.multiply(number1, number2);;
+            break;
+            case '/':
+            calc.divide(number1, number2);;
+            break;
+            case '^':
+            calc.power(number1, number2);
+            break;
+            default:
+            alert('Dzialanie nie istnieje!');
+            break;
         }
     }
-    
+
 } while(calc.isCorrectAction(action));
+
