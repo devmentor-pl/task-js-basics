28d6f6e88e6ac99e0bac8d012b2979e0887bccd1
Max <maciejdobisz@gmail.com>
2022-01-24 14:23:58 +0000

    task js basics

--- js/app.js
+++ js/app.js
@@ -7,53 +7,51 @@ Calculator.prototype.isCorrectAction = function(action) {
     return this.actions.includes(action);
 }
 
+Calculator.prototype.isNumber = function (input1, input2) {
+    return typeof input1 === 'number' && typeof input2 === 'number';
+}
+
 Calculator.prototype.getHistoryAsString = function() {
     return this.history.join('\n');
 }
 
 Calculator.prototype.add = function(num1, num2) {
-    const x=parseInt(num1);
-    const y=parseInt(num2);
-    if(typeof x==='number'&&typeof y==='number') {
-        const result = x+y;
-        this.history.push(x+'+'+y+'='+result);
+    if(this.isNumber(num1, num2)) {
+        const result = num1 + num2;
+        this.history.push(num1 + '+' + num2 + '=' + result);
     }
 }
 
 Calculator.prototype.subtract = function(num1, num2) {
-    const x=parseInt(num1);
-    const y=parseInt(num2);
-    if(typeof x==='number'&&typeof y==='number') {
-        const result = x - y;
-        this.history.push(x+'-'+y+'='+result);
+    if(this.isNumber(num1, num2)) {
+        const result = num1 - num2;
+        this.history.push(num1 + '-' + num2 + '=' + result);
     }
 }
 
 Calculator.prototype.multiply = function(num1, num2) {
-    const x=parseInt(num1);
-    const y=parseInt(num2);
-    if(typeof x==='number'&&typeof y==='number') {
-        const result = x * y;
-        this.history.push(x+'*'+y+'='+result);
+    if(this.isNumber(num1, num2)) {
+        const result = num1 * num2;
+        this.history.push(num1 + '*' + num2 + '=' + result);
     }
 }
 
 Calculator.prototype.divide = function(num1, num2) {
-    const x=parseInt(num1);
-    const y=parseInt(num2);
-    if(typeof x==='number'&&typeof y==='number') {
-        const result = x / y;
-        this.history.push(x+'/'+y+'='+result);
+    if(this.isNumber(num1, num2)) {
+        const result = num1 / num2;
+        this.history.push(num1 + '/' + num2 + '=' + result);
     }
 }
 
 Calculator.prototype.power = function(num1, num2) {
-    const x=parseInt(num1);
-    const y=parseInt(num2);
-    if(typeof x==='number'&&typeof y==='number') {
-        let result = x;
-        if(y!==0) {for(let i=2; i<=y; i++) {result*=x;}this.history.push(x+'^'+y+'='+result);} else {
-            this.history.push(x+'^'+y+'='+1);
+    if(this.isNumber(num1, num2)) {
+        let result = num1;
+        if(num2 !== 0) {
+            for(let i=2; i<=num2; i++) {
+                result *= num1;}
+            this.history.push(num1 + '^' + num2 + '=' + result);
+        } else {
+            this.history.push(num1 + '^' + num2 + '=' + 1);
         }
     }
 }
@@ -66,8 +64,8 @@ do {
     action = prompt(promptContent);
     isCorrectAction = calc.isCorrectAction(action);
     if(isCorrectAction) {
-        number1 = prompt('Podaj liczbę nr 1');
-        number2 = prompt('Podaj liczbę nr 2');
+        number1 = parseInt(prompt('Podaj liczbę nr 1'));
+        number2 = parseInt(prompt('Podaj liczbę nr 2'));
         switch(action) {
             case '+':
             calc.add(number1, number2);
@@ -91,4 +89,3 @@ do {
     }
 
 } while(calc.isCorrectAction(action));
-
