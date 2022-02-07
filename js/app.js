5569d33464f5b267541ca2eacdf42f0f753ff946
Tomasz Cividino <tomaszcividino@gmail.com>
2022-02-07 14:15:57 +0100

    Calculator refactor JS

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-
-
 function Calculator() {
     this.actions = ['+', '-', '*', '/', '^'];
     this.history = [];
@@ -17,91 +15,77 @@ Calculator.prototype.addToHistory = function(value) {
   return this.history.push(value)
 }
 
-Calculator.prototype.add = function(num1, num2) { 
-  const parsedNum1 = parseInt(num1);
-  const parsedNum2 = parseInt(num2);
-    if (!isNaN(parsedNum1) && !isNaN(parsedNum2)) {
-        const parsedSum =  parsedNum1 + parsedNum2
-        calc.addToHistory(parsedNum1 + ' + ' + parsedNum2 + ' = ' + parsedSum);
-      }
+Calculator.prototype.add = function(num1, num2) {
+  const parsedSum =  num1 + num2
+  calc.addToHistory(num1 + ' + ' + num2 + ' = ' + parsedSum);
   }
 
 Calculator.prototype.diff = function(num1, num2) {
-    const parsedNum1 = parseInt(num1);
-    const parsedNum2 = parseInt(num2);
-    if (!isNaN(parsedNum1) && !isNaN(parsedNum2)) {
-        const parsedSum =  parsedNum1 - parsedNum2
-        calc.addToHistory(parsedNum1 + ' - ' + parsedNum2 + ' = ' + parsedSum);
-      }
+  const parsedSum =  num1 - num2
+  calc.addToHistory(num1 + ' - ' + num2 + ' = ' + parsedSum);
   }
 
 Calculator.prototype.ratio = function(num1, num2) {
-    const parsedNum1 = parseInt(num1);
-    const parsedNum2 = parseInt(num2);
-    if (!isNaN(parsedNum1) && !isNaN(parsedNum2)) {
-        const parsedSum =  parsedNum1 * parsedNum2
-        calc.addToHistory(parsedNum1 + ' * ' + parsedNum2 + ' = ' + parsedSum);
-      }
+  const parsedSum =  num1 * num2
+  calc.addToHistory(num1 + ' * ' + num2 + ' = ' + parsedSum);
   }
 
 Calculator.prototype.division = function(num1, num2) {
-    const parsedNum1 = parseInt(num1);
-    const parsedNum2 = parseInt(num2);
-    if (!isNaN(parsedNum1) && !isNaN(parsedNum2)) {
-        const parsedSum =  parsedNum1 / parsedNum2
-        calc.addToHistory(parsedNum1 + ' / ' + parsedNum2 + ' = ' + parsedSum);
-      }
+  const parsedSum =  num1 / num2
+  calc.addToHistory(num1 + ' / ' + num2 + ' = ' + parsedSum);
   }
 
 Calculator.prototype.expo = function(num1, num2) {
-    const parsedNum1 = parseInt(num1);
-    const parsedNum2 = parseInt(num2);
-    if (!isNaN(parsedNum1) && !isNaN(parsedNum2)) {
-        let i = 1;
-        let parsedSum = 1;
-          while (i <= parsedNum2) {
-            i++;
-            parsedSum = parsedSum * parsedNum1
-          }
-        calc.addToHistory(parsedNum1 + ' ^ ' + parsedNum2 + ' = ' + parsedSum);
-      }
+  let i = 1;
+  let parsedSum = 1;
+  while (i <= num2) {
+    i++;
+    parsedSum = parsedSum * num1
   }
+  calc.addToHistory(num1 + ' ^ ' + num2 + ' = ' + parsedSum);
+ }
 
 const calc = new Calculator();
 let action, promptContent, isCorrectAction, number1, number2;
 do { 
-    promptContent = 'Podaj jaką operację chcesz wykonać (+, -, *, /, ^) i potwierdź. \n'; // \n - znak nowej linii
+    promptContent = 'Podaj jaką operację chcesz wykonać (+, -, *, /,     ^) i potwierdź. \n'; // \n - znak nowej linii
     promptContent += 'Jeśli chcesz zrezygnować wciśnij Anuluj. \n';
-    promptContent += 'Lista poprzednich operacji: \n' + calc.getHistoryAsString();
+    promptContent += 'Lista poprzednich operacji: \n' +              calc.getHistoryAsString();
 
     action = prompt(promptContent);
     isCorrectAction = calc.isCorrectAction(action);
     if(isCorrectAction) {
         number1 = prompt('Podaj liczbę nr 1');
         number2 = prompt('Podaj liczbę nr 2');
+      
+        const parsedNum1 = parseInt(number1);
+        const parsedNum2 = parseInt(number2);
+        
+        if(!isNaN(parsedNum1) && !isNaN(parsedNum2)) {
+          if(action === '+') {
+              calc.add(parsedNum1, parsedNum2);
+          }
 
-        if(action === '+') {
-            calc.add(number1, number2);
+          if(action === '-') {
+              calc.diff(parsedNum1, parsedNum2);
+           }
+
+          if(action === '*') {
+              calc.ratio(parsedNum1, parsedNum2);
+           }
+
+          if(action === '/') {
+              calc.division(parsedNum1, parsedNum2);
+           }
+
+          if(action === '^') {
+              calc.expo(parsedNum1, parsedNum2);
+           }
         }
-      
-        if(action === '-') {
-            calc.diff(number1, number2);
-         }
-      
-        if(action === '*') {
-            calc.ratio(number1, number2);
-         }
-      
-        if(action === '/') {
-            calc.division(number1, number2);
-         }
-      
-        if(action === '^') {
-            calc.expo(number1, number2);
-         }
     }
     
 } while(calc.isCorrectAction(action));
 
 console.log(calc.history)
+
 
