a33a08e9347f02f6b12e9dd499283b6cd0337995
Jakub Parol <kubciop33@gmail.com>
2022-03-04 14:35:39 +0100

    Add functionality to the calculator

--- js/app.js
+++ js/app.js
@@ -16,10 +16,11 @@ Calculator.prototype.add = function(num1, num2) {
     // 2. sprawdź czy są one poprawne
     // 3. jeśli tak to wykonaj działanie i zapisz jego resultat
     // 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
+    this.history.push(num1 + ' ' + action + ' ' + num2 + ' = ' + result);
 }
 
 const calc = new Calculator();
-let action, promptContent, isCorrectAction, number1, number2;
+let action, promptContent, isCorrectAction, number1, number2, result;
 do { 
     promptContent = 'Podaj jaką operację chcesz wykonać (+, -, *, /, ^) i potwierdź. \n'; // \n - znak nowej linii
     promptContent += 'Jeśli chcesz zrezygnować wciśnij Anuluj. \n';
@@ -28,11 +29,32 @@ do {
     action = prompt(promptContent);
     isCorrectAction = calc.isCorrectAction(action);
     if(isCorrectAction) {
-        number1 = prompt('Podaj liczbę nr 1');
-        number2 = prompt('Podaj liczbę nr 2');
-
-        if(action === '+') {
-            calc.add(number1, number2);
+        number1 = parseInt(prompt('Podaj liczbę nr 1'));
+        number2 = parseInt(prompt('Podaj liczbę nr 2'));
+        if(typeof number1 === 'number' && typeof number2 === 'number') {
+            if(action === '+') {
+                result = number1 + number2;
+                calc.add(number1, number2);
+            }
+            if(action === '-') {
+                result = number1 - number2;
+                calc.add(number1, number2);
+            }
+            if(action === '*') {
+                result = number1 * number2;
+                calc.add(number1, number2);
+            }
+            if(action === '/') {
+                result = number1 / number2;
+                calc.add(number1, number2);
+            }
+            if(action === '^') {
+                result = 1;
+                for(let i=1; i<=number2; i++){
+                    result = result * number1;
+                }
+                calc.add(number1, number2);
+            }
         }
     }
     
