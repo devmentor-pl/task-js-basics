7c61cf46a8b466eba43260771c0eeff07167a14f
Mateusz Suplewski <matx3582@gmail.com>
2022-04-01 21:02:02 +0200

    Basic calculator - Refactored

--- js/app.js
+++ js/app.js
@@ -1,93 +1,54 @@
 function Calculator() {
-
     this.actions = ['+', '-', '*', '/', '^'];
     this.history = [];
-
 }
 
-Calculator.prototype.isCorrectAction = function (action) {
-    return this.actions.includes(action);
-}
+Calculator.prototype.isCorrectAction = function (action) { return this.actions.includes(action); }
 
-Calculator.prototype.getHistoryAsString = function () {
-    return this.history.join('\n');
-}
+Calculator.prototype.getHistoryAsString = function () { return this.history.join('\n'); }
 
-Calculator.prototype.add = function (num1, num2) {
-    const number1 = Number(num1);
-    const number2 = Number(num2);
-    let result = 0;
+Calculator.prototype.add = function (num1, num2) { return num1 + num2; }
 
-    if (!Number.isNaN(number1) && !Number.isNaN(number2)) {
-        result = number1 + number2;
-        this.history.push(number1 + ' + ' + number2 + ' = ' + result);
-        return result;
-    }
-    // 1. zamień wartości przekazane przez parametr na typ number
-    // 2. sprawdź czy są one poprawne
-    // 3. jeśli tak to wykonaj działanie i zapisz jego resultat
-    // 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
-}
+Calculator.prototype.subtract = function (num1, num2) { return num1 - num2; }
 
-Calculator.prototype.subtract = function (num1, num2) {
-    const number1 = Number(num1);
-    const number2 = Number(num2);
-    let result = 0;
-    if (!Number.isNaN(number1) && !Number.isNaN(number2)) {
-        result = number1 - number2;
-        this.history.push(number1 + ' - ' + number2 + ' = ' + result);
-        return result;
-    }
+Calculator.prototype.divide = function (num1, num2) { return num1 / num2; }
 
+Calculator.prototype.multiply = function (num1, num2) { return num1 * num2; }
 
-}
-
-Calculator.prototype.divide = function (num1, num2) {
-    const number1 = Number(num1);
-    const number2 = Number(num2);
-    let result = 0;
-    if (!Number.isNaN(number1) && !Number.isNaN(number2)) {
-        result = number1 / number2;
-        this.history.push(number1 + ' / ' + number2 + ' = ' + result);
-        return result;
-    }
-
-
-}
-
-Calculator.prototype.multiply = function (num1, num2) {
-    const number1 = Number(num1);
-    const number2 = Number(num2);
-    let result = 0;
-    if (!Number.isNaN(number1) && !Number.isNaN(number2)) {
-        result = number1 * number2;
-        this.history.push(number1 + ' * ' + number2 + ' = ' + result);
-        return result;
-    }
-}
-
-Calculator.prototype.magnify = function (num1, num2) {
-    const number = Number(num1);
-    const power = Number(num2);
+Calculator.prototype.magnify = function (num, power) {
     let result = 1;
-    if (!Number.isNaN(number) && !Number.isNaN(power)) {
-        for (let i = 0; i < power; i++) {
-            result *= number;
-        }
-        this.history.push(number + ' ^ ' + power + ' = ' + result);
-        return result;
-    }
+    for (let i = 0; i < power; i++) result *= num;
+    return result;
 }
 
+Calculator.prototype.calculate = function (operator, num1, num2) {
+    let result = 0;
+    switch (operator) {
+        case '+':
+            result = this.add(num1, num2);
+            break;
+        case '-':
+            result = this.subtract(num1, num2);
+            break;
+        case '/':
+            result = this.divide(num1, num2);
+            break;
+        case '*':
+            result = this.multiply(num1, num2);
+            break;
+        case '^':
+            result = this.magnify(num1, num2);
+            break;
+    }
+    this.history.push(number1 + ' ' + operator + ' ' + number2 + ' = ' + result);
+}
 
 
 const calc = new Calculator();
-
 let action, promptContent, isCorrectAction, number1, number2;
 
 do {
-
-    promptContent = 'Podaj jaką operację chcesz wykonać (+, -, *, /, ^) i potwierdź. \n'; // \n - znak nowej linii
+    promptContent = 'Podaj jaką operację chcesz wykonać (+, -, *, /, ^) i potwierdź. \n';
     promptContent += 'Jeśli chcesz zrezygnować wciśnij Anuluj. \n';
     promptContent += 'Lista poprzednich operacji: \n' + calc.getHistoryAsString();
 
@@ -95,28 +56,10 @@ do {
     isCorrectAction = calc.isCorrectAction(action);
 
     if (isCorrectAction) {
+        number1 = Number(prompt('Podaj liczbę nr 1'));
+        number2 = Number(prompt('Podaj liczbę nr 2'));
 
-
-        number1 = prompt('Podaj liczbę nr 1');
-        number2 = prompt('Podaj liczbę nr 2');
-
-        if (action === '+') {
-            calc.add(number1, number2);
-        }
-        else if (action === '-') {
-            calc.subtract(number1, number2);
-        }
-        else if (action === '/') {
-            calc.divide(number1, number2);
-        }
-        else if (action === '*') {
-            calc.multiply(number1, number2);
-        }
-        else if (action === '^') {
-            calc.magnify(number1, number2);
-        }
-
-
+        if (!Number.isNaN(number1) && !Number.isNaN(number2)) calc.calculate(action, number1, number2);
     }
 
 } while (calc.isCorrectAction(action));
