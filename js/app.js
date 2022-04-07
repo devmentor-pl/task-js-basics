d62b6609572c8b67eba8cbf32c7f16a8ff889fbf
Camila Montes Celinski <camicelinski@gmail.com>
2022-04-07 03:09:01 +0200

    Complete js-basics task

--- js/app.js
+++ js/app.js
@@ -11,11 +11,52 @@ Calculator.prototype.getHistoryAsString = function() {
     return this.history.join('\n');
 }
 
-Calculator.prototype.add = function(num1, num2) {
+Calculator.prototype.operation = function(num1, num2, action) {
     // 1. zamień wartości przekazane przez parametr na typ number
+    num1 = Number(num1);
+    num2 = Number(num2);
     // 2. sprawdź czy są one poprawne
-    // 3. jeśli tak to wykonaj działanie i zapisz jego resultat
-    // 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
+    // 3. jeśli tak to wykonaj działanie i zapisz jego rezultat
+    // 4. dodaj do historii operacji to działanie w formie: 1 + 1 = 2    
+    if(!isNaN(num1) && !isNaN(num2)) {
+        let result;
+        switch(action) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if(num2 !== 0) {
+                    result = num1 / num2;
+                } else {
+                    result = 'Nie można dzielić przez zero'
+                }
+                break;
+            case '^':
+                if(num2 === 0) {
+                    result = 1;            
+                } else {
+                    i = 1;
+                    result = 1;
+                    while(i<=Math.abs(num2)) {
+                        result *= num1;
+                        i++;
+                    }                    
+                    if(num2 < 0) {
+                        result = 1/result; 
+                    }                    
+                }     
+                break;  
+        } 
+            this.history.push(`${num1} ${action} ${num2} = ${result}`);
+    } else {
+        alert('Podałeś błędne dane. Próbuj jeszcze raz!');
+    }
 }
 
 const calc = new Calculator();
@@ -28,12 +69,13 @@ do {
     action = prompt(promptContent);
     isCorrectAction = calc.isCorrectAction(action);
     if(isCorrectAction) {
+
         number1 = prompt('Podaj liczbę nr 1');
         number2 = prompt('Podaj liczbę nr 2');
 
-        if(action === '+') {
-            calc.add(number1, number2);
-        }
+        calc.operation(number1, number2, action);        
+    } else {
+        alert('Podałeś błędne dane. Odśwież stronę i próbuj ponownie!');
     }
     
 } while(calc.isCorrectAction(action));
