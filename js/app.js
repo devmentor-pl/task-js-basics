69dcf731b936dcdf3869b24218f089631ec97ed4
Dawid Drag <dede7d@wp.pl>
2022-04-16 22:03:07 +0200

    added js code

--- js/app.js
+++ js/app.js
@@ -3,37 +3,193 @@ function Calculator() {
     this.history = [];
 }
 
-Calculator.prototype.isCorrectAction = function(action) {
+Calculator.prototype.isCorrectAction = function (action) {
     return this.actions.includes(action);
 }
 
-Calculator.prototype.getHistoryAsString = function() {
+Calculator.prototype.getHistoryAsString = function () {
     return this.history.join('\n');
 }
 
-Calculator.prototype.add = function(num1, num2) {
+Calculator.prototype.add = function (num1, num2) {
+
+    const number1 = Number(num1);
+    const number2 = Number(num2);
+
+    if (isNaN(number1)) {
+        alert('Wpisano złe dane!');
+    }
+
+    else if (isNaN(number2)) {
+        alert('Wpisano złe dane!');
+    }
+
+    else {
+
+
+        let result = number1 + number2;
+        let info = number1 + ' + ' + number2 + ' = ' + result;
+
+        this.history.push(info);
+
+        return result;
+    }
+
     // 1. zamień wartości przekazane przez parametr na typ number
     // 2. sprawdź czy są one poprawne
     // 3. jeśli tak to wykonaj działanie i zapisz jego resultat
     // 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
 }
 
+
+Calculator.prototype.subtraction = function (num1, num2) {
+
+    const number1 = Number(num1);
+    const number2 = Number(num2);
+
+    if (isNaN(number1)) {
+        alert('Wpisano złe dane!');
+    }
+
+    else if (isNaN(number2)) {
+        alert('Wpisano złe dane!');
+    }
+
+    else {
+
+
+        let result = number1 - number2;
+        let info = number1 + ' - ' + number2 + ' = ' + result;
+
+        this.history.push(info);
+
+        return result;
+    }
+
+}
+
+
+Calculator.prototype.multiplication = function (num1, num2) {
+
+    const number1 = Number(num1);
+    const number2 = Number(num2);
+
+    if (isNaN(number1)) {
+        alert('Wpisano złe dane!');
+    }
+
+    else if (isNaN(number2)) {
+        alert('Wpisano złe dane!');
+    }
+
+    else {
+        let result = number1 * number2;
+        let info = number1 + ' * ' + number2 + ' = ' + result;
+
+        this.history.push(info);
+        return result;
+
+    }
+
+
+}
+
+
+Calculator.prototype.division = function (num1, num2) {
+
+    const number1 = Number(num1);
+    const number2 = Number(num2);
+
+    if (isNaN(number1)) {
+        alert('Wpisano złe dane!');
+    }
+
+    else if (isNaN(number2)) {
+        alert('Wpisano złe dane!');
+    }
+
+    else {
+        let result = number1 / number2;
+        let info = number1 + ' / ' + number2 + ' = ' + result;
+
+        this.history.push(info);
+        return result;
+
+    }
+
+
+}
+
+Calculator.prototype.exponentiation = function (num1, num2) {
+
+    const number1 = Number(num1);
+    const number2 = Number(num2);
+
+    if (isNaN(number1)) {
+        alert('Wpisano złe dane!');
+    }
+
+    else if (isNaN(number2)) {
+        alert('Wpisano złe dane!');
+    }
+
+    else {
+
+        let result = number1;
+
+        for (let i = 1; i < number2; i++) {
+            result = result * number1;
+        }
+
+
+
+        let info = number1 + ' ^ ' + number2 + ' = ' + result;
+
+        this.history.push(info);
+        return result;
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
 const calc = new Calculator();
 let action, promptContent, isCorrectAction, number1, number2;
-do { 
+do {
     promptContent = 'Podaj jaką operację chcesz wykonać (+, -, *, /, ^) i potwierdź. \n'; // \n - znak nowej linii
     promptContent += 'Jeśli chcesz zrezygnować wciśnij Anuluj. \n';
     promptContent += 'Lista poprzednich operacji: \n' + calc.getHistoryAsString();
 
     action = prompt(promptContent);
     isCorrectAction = calc.isCorrectAction(action);
-    if(isCorrectAction) {
+    if (isCorrectAction) {
         number1 = prompt('Podaj liczbę nr 1');
         number2 = prompt('Podaj liczbę nr 2');
 
-        if(action === '+') {
+        if (action === '+') {
             calc.add(number1, number2);
         }
+        else if (action === '-') {
+            calc.subtraction(number1, number2);
+        }
+        else if (action === '*') {
+            calc.multiplication(number1, number2);
+        }
+        else if (action === '/') {
+            calc.division(number1, number2);
+        }
+        else if (action === '^') {
+            calc.exponentiation(number1, number2);
+        }
+
+
     }
-    
-} while(calc.isCorrectAction(action));
+
+} while (calc.isCorrectAction(action));
