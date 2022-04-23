f285d686b2140af19add5f6b6ee0f9f4eb11540f
Marcin Frackowiak <frackowiak.martin@gmail.com>
2022-04-24 00:07:52 +0200

    first point

--- js/app.js
+++ js/app.js
@@ -3,6 +3,11 @@ function Calculator() {
     this.history = [];
 }
 
+Calculator.prototype.alert = function(){
+    return console.log('Wprowadzono niewłaściwą wartość \n Spróbuj jeszcze raz')
+}
+
+
 Calculator.prototype.isCorrectAction = function(action) {
     return this.actions.includes(action);
 }
@@ -16,6 +21,73 @@ Calculator.prototype.add = function(num1, num2) {
     // 2. sprawdź czy są one poprawne
     // 3. jeśli tak to wykonaj działanie i zapisz jego resultat
     // 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
+    let value1 = Number(num1)
+    let value2 = Number(num2)
+    if(!Number.isNaN(value1) && !Number.isNaN(value2)) {
+        let result = value1 + value2
+        this.history.push(value1 + ' + ' + value2 + ' = ' + result)
+        // this.getHistoryAsString(value1 + '+' + value2 + '=' + result)
+        return result
+    } else {
+        this.alert()
+    }
+
+}
+
+Calculator.prototype.subtract = function(num1, num2) {
+
+    let value1 = Number(num1)
+    let value2 = Number(num2)
+    if(!Number.isNaN(value1) && !Number.isNaN(value2)) {
+        let result = value1 - value2
+        this.history.push(value1 + ' - ' + value2 + ' = ' + result)
+        return result
+    } else {
+        this.alert()
+    }
+
+}
+
+Calculator.prototype.multiply = function(num1, num2) {
+    let value1 = Number(num1)
+    let value2 = Number(num2)
+    if(!Number.isNaN(value1) && !Number.isNaN(value2)) {
+        let result = value1 * value2
+        this.history.push(value1 + ' * ' + value2 + ' = ' + result)
+        return result
+    } else {
+        this.alert()
+    }
+
+}
+
+Calculator.prototype.divide = function(num1, num2) {
+    let value1 = Number(num1)
+    let value2 = Number(num2)
+    if(!Number.isNaN(value1) && !Number.isNaN(value2) && value2 !== 0) {
+        let result = value1 / value2
+        this.history.push(value1 + ' / ' + value2 + ' = ' + result)
+        return result
+    } else {
+        this.alert()
+    }
+
+}
+
+Calculator.prototype.exponentiation = function(num1, num2) {
+    let value1 = Number(num1)
+    let value2 = Number(num2)
+    if(!Number.isNaN(value1) && !Number.isNaN(value2) && value2 >=1) {
+        let result = value1
+        for(let i = 1; i < value2; i++) {
+            result *= value1
+        }
+        this.history.push(value1 + ' ^ ' + value2 + ' = ' + result)
+        return result
+    } else {
+        this.alert()
+    }
+
 }
 
 const calc = new Calculator();
@@ -33,7 +105,15 @@ do {
 
         if(action === '+') {
             calc.add(number1, number2);
-        }
+        }else if(action === '-') {
+            calc.subtract(number1, number2)
+        }else if(action === '*') {
+            calc.multiply(number1, number2)
+        }else if(action === '/') {
+            calc.divide(number1, number2)
+        }else if(action === '^') {
+            calc.exponentiation(number1, number2)
+        } 
     }
     
 } while(calc.isCorrectAction(action));
