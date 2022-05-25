7f36901882e40f45d81f0142a1b7edc9c3d1531a
Angelika Rybicka <rybicka.angelika@gmail.com>
2022-05-25 19:09:13 +0200

    Finish calculator

--- js/app.js
+++ js/app.js
@@ -11,11 +11,57 @@ Calculator.prototype.getHistoryAsString = function() {
     return this.history.join('\n');
 }
 
+Calculator.prototype.addToHistory = function (result) {
+    return this.history.push(result)
+}
+
+Calculator.prototype.displayResult = function (res) {
+    return result = number1 + " " + action + " " + number2 + ' = ' + res
+}
+
 Calculator.prototype.add = function(num1, num2) {
     // 1. zamień wartości przekazane przez parametr na typ number
     // 2. sprawdź czy są one poprawne
-    // 3. jeśli tak to wykonaj działanie i zapisz jego resultat
-    // 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
+    // 3. jeśli tak to wykonaj działanie i zapisz jego rezultat
+    // 4. dodaj do historii operacji to działanie w formie: 1 + 1 = 2
+    const sum = num1 + num2
+    calc.displayResult(sum)
+    calc.addToHistory(result)
+}
+
+Calculator.prototype.subtract = function(num1, num2) {
+    const sub = num1 - num2
+    calc.displayResult(sub)
+    calc.addToHistory(result)
+}
+
+Calculator.prototype.multiply = function(num1, num2) {
+    const multi = num1 * num2
+    calc.displayResult(multi)
+    calc.addToHistory(result)
+}
+
+Calculator.prototype.divide = function(num1, num2) {
+    if (num2 === 0) {
+        alert('Nie dzieli się przez 0.')
+    } else {
+        const div = num1 / num2
+        calc.displayResult(div)
+        calc.addToHistory(result)
+    }
+}
+
+Calculator.prototype.pow = function(num1, num2) {
+    let pow = 1
+    if (num2 === 0) {
+        pow = 1;
+    } else {
+        for(let i=1; i<=num2; i++) {
+        pow *= num1
+        }
+    }
+    calc.displayResult(pow)
+    calc.addToHistory(result)
 }
 
 const calc = new Calculator();
@@ -28,12 +74,31 @@ do {
     action = prompt(promptContent);
     isCorrectAction = calc.isCorrectAction(action);
     if(isCorrectAction) {
-        number1 = prompt('Podaj liczbę nr 1');
-        number2 = prompt('Podaj liczbę nr 2');
+        number1 = Number(prompt('Podaj liczbę nr 1'))
+        number2 = Number(prompt('Podaj liczbę nr 2'))
 
+        if (isNaN(number1) || isNaN(number2)) {
+            alert('Podałeś błędne dane. Spróbuj ponownie.')
+        } else
         if(action === '+') {
-            calc.add(number1, number2);
+            calc.add(number1, number2)
+        } else 
+        if(action === '-') {
+            calc.subtract(number1, number2)
+        } else 
+        if(action === '*') {
+            calc.multiply(number1, number2)
+        } else 
+        if(action === '/') {
+            calc.divide(number1, number2)
+        } else 
+        if(action === '^') {
+            calc.pow(number1, number2)
         }
-    }
+    } else 
+    if (action === null) {  
+    } else {
+        alert('Nieprawidłowy operator działania.')
+    } 
     
 } while(calc.isCorrectAction(action));
