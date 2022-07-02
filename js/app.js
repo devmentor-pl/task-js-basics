3c4b4a0fefe46a33eba8948927df0acb6b2d8411
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2022-07-02 09:45:39 +0200

    change to Number and check if num1 and num2 is a Number

--- js/app.js
+++ js/app.js
@@ -7,18 +7,58 @@ Calculator.prototype.isCorrectAction = function(action) {
     return this.actions.includes(action);
 }
 
+const action1 = new Calculator()
+console.log(action1.isCorrectAction('+'))
+
+
 Calculator.prototype.getHistoryAsString = function() {
     return this.history.join('\n');
 }
 
 Calculator.prototype.add = function(num1, num2) {
     // 1. zamień wartości przekazane przez parametr na typ number
+     //const changeToNumber = Number(num1, num2)
+    
+    
     // 2. sprawdź czy są one poprawne
+    
     // 3. jeśli tak to wykonaj działanie i zapisz jego resultat
     // 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
+
+// function getNumberFromUser(num1, num2 ){
+//         num1 = prompt('Podaj liczbę nr 1');
+//         num2 = prompt('Podaj liczbę nr 2');
+    let number1 = Number(num1)
+    let number2 = Number(num2)
+
+    const result = number1 + number2
+
+    return result
+
+        
+        
+//         if(Number.isNaN(number1)) {
+//             return getNumberFromUser()
+//         }else {
+//             return number1
+//         }
+
+//         if(Number.isNaN(number2)) {
+//             return getNumberFromUser()
+//         }else {
+//             return number2
+//         }
+        
 }
 
+const action2 = new Calculator()
+console.log(action2.add(2,6))
+console.log(6 === Number.isNaN)
+
+//Calculator.prototype.
+
 const calc = new Calculator();
+
 let action, promptContent, isCorrectAction, number1, number2;
 do { 
     promptContent = 'Podaj jaką operację chcesz wykonać (+, -, *, /, ^) i potwierdź. \n'; // \n - znak nowej linii
@@ -28,12 +68,31 @@ do {
     action = prompt(promptContent);
     isCorrectAction = calc.isCorrectAction(action);
     if(isCorrectAction) {
-        number1 = prompt('Podaj liczbę nr 1');
-        number2 = prompt('Podaj liczbę nr 2');
+        
+        num1 = prompt('Podaj liczbę nr 1');
+        
+        if(Number.isNaN(num1)) {
+            
+        } else {
+            prompt('Podaj liczbę nr 1')
+        }
+
+        num2 = prompt('Podaj liczbę nr 2') 
+
+        if(Number.isNaN(num1)) {
+            
+        } else {
+            prompt('Podaj liczbę nr 2')
+        }
+       
+        //             }else if {
+        //                 num2 = prompt('Podaj liczbę nr 2')  
+        //             }
+        
 
         if(action === '+') {
             calc.add(number1, number2);
         }
     }
     
-} while(calc.isCorrectAction(action));
+} while(calc.isCorrectAction(action))
