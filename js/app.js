c6fe635c9336d3e05abad660009a58c8863bed37
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2022-07-02 12:08:12 +0200

    add metod power to Calculator

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 function Calculator() {
-    this.actions = ['+', '-', '*', '/', '^'];
+    this.actions = ['+', '-', '*', '/', '**'];
     this.history = [];
 }
 
@@ -24,13 +24,49 @@ Calculator.prototype.add = function(num1, num2) {
 
     const result = parseFloat(num1) + parseFloat(num2)
     return console.log(result)
-
-
 }
-const calc = new Calculator();
-const action2 = calc.add()
-console.log(action2)
 
+Calculator.prototype.substract = function(num1, num2) {
+    
+
+    const result = parseFloat(num1) - parseFloat(num2)
+    return console.log(result)
+}
+
+Calculator.prototype.multiply = function(num1, num2) {
+    
+
+    const result = parseFloat(num1) * parseFloat(num2)
+    return console.log(result)
+}
+
+Calculator.prototype.divide = function(num1, num2) {
+    
+
+    const result = parseFloat(num1) / parseFloat(num2)
+    return console.log(result)
+}
+
+Calculator.prototype.power = function(number, power) {
+    
+        if (power == 0){
+            return 1;
+            }
+        let temp = number;
+        let result = number
+        for (i = 0; i < power - 1; i++){
+            result = result * temp;
+        }
+        return console.log(result)
+}
+
+
+    
+
+
+
+
+const calc = new Calculator();
 let action, promptContent, isCorrectAction, number1, number2;
 do { 
     promptContent = 'Podaj jaką operację chcesz wykonać (+, -, *, /, ^) i potwierdź. \n'; // \n - znak nowej linii
@@ -41,28 +77,122 @@ do {
     isCorrectAction = calc.isCorrectAction(action);
     if(isCorrectAction) {
         
-        number1 = prompt('Podaj liczbę nr 1');
-        
-        if(Number.isNaN(number1)) {
-            prompt('Podaj liczbę nr 1')
-        } else {
-            let num1;
-            num1 = Number(number1)
+       
+       
+       if(action === '+') {
             
-        }
+            number1 = prompt('Podaj liczbę nr 1');
+        
+            if(Number.isNaN(number1)) {
+                prompt('Podaj liczbę nr 1')
+            } else {
+                let num1;
+                num1 = Number(number1)
+                
+            }
 
-        number2 = prompt('Podaj liczbę nr 2') 
+            number2 = prompt('Podaj liczbę nr 2') 
 
-        if(Number.isNaN(number2)) {
-            prompt('Podaj liczbę nr 2')
-        } else {
-            let num2;
-            num2 = Number(number2)
-        }
-       
-       
-        if(action === '+') {
+            if(Number.isNaN(number2)) {
+                prompt('Podaj liczbę nr 2')
+            } else {
+                let num2;
+                num2 = Number(number2)
+            }
+
             calc.add(number1, number2);
+        }
+
+        if(action === '-') {
+
+            number1 = prompt('Podaj liczbę nr 1');
+        
+            if(Number.isNaN(number1)) {
+                prompt('Podaj liczbę nr 1')
+            } else {
+                let num1;
+                num1 = Number(number1)
+                
+            }
+
+            number2 = prompt('Podaj liczbę nr 2') 
+
+            if(Number.isNaN(number2)) {
+                prompt('Podaj liczbę nr 2')
+            } else {
+                let num2;
+                num2 = Number(number2)
+            }
+
+            calc.substract(number1, number2);
+        }
+
+        if(action === '*') {
+
+            number1 = prompt('Podaj liczbę nr 1');
+        
+            if(Number.isNaN(number1)) {
+                prompt('Podaj liczbę nr 1')
+            } else {
+                let num1;
+                num1 = Number(number1)
+                
+            }
+
+            number2 = prompt('Podaj liczbę nr 2') 
+
+            if(Number.isNaN(number2)) {
+                prompt('Podaj liczbę nr 2')
+            } else {
+                let num2;
+                num2 = Number(number2)
+            }
+
+            calc.multiply(number1, number2);
+        }
+
+        if(action === '/') {
+            number1 = prompt('Podaj liczbę nr 1');
+        
+            if(Number.isNaN(number1)) {
+                prompt('Podaj liczbę nr 1')
+            } else {
+                let num1;
+                num1 = Number(number1)
+                
+            }
+
+            number2 = prompt('Podaj liczbę nr 2') 
+
+            if(Number.isNaN(number2)) {
+                prompt('Podaj liczbę nr 2')
+            } else {
+                let num2;
+                num2 = Number(number2)
+            }
+
+            calc.divide(number1, number2);
+        }
+
+        if(action === '**') {
+
+            number = prompt('Podaj liczbę')
+            if(Number.isNaN(number)) {
+                prompt('Podaj liczbę nr 1')
+            } else {
+                let number1;
+                number1 = Number(number)
+                
+            }
+            power = prompt('Podaj potęgę')
+            if(Number.isNaN(power)) {
+                prompt('Podaj liczbę nr 1')
+            } else {
+                let power1;
+                power1 = Number(power)
+            }
+
+            calc.power(number, power);
         }
 
     }
