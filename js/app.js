2d2bd62d2619b96f48305ebd06b67680a582b339
Camila Montes Celinski <camicelinski@gmail.com>
2022-07-05 22:55:14 +0200

    Correct after code review

--- js/app.js
+++ js/app.js
@@ -1,61 +1,73 @@
 function Calculator() {
-    this.actions = ['+', '-', '*', '/', '^'];
+    this.actions = {
+        '+': this.add,
+        '-': this.subtract,
+        '*': this.multiply,
+        '/': this.divide,
+        '^': this.exponential
+    };
     this.history = [];
 }
 
 Calculator.prototype.isCorrectAction = function(action) {
-    return this.actions.includes(action);
+    return this.actions.hasOwnProperty(action);
 }
 
 Calculator.prototype.getHistoryAsString = function() {
     return this.history.join('\n');
 }
 
-Calculator.prototype.operation = function(num1, num2, action) {
+Calculator.prototype.add = function(a, b) {
+    return a + b
+}
+
+Calculator.prototype.subtract = function(a, b) {
+    return a - b    
+}
+
+Calculator.prototype.multiply = function(a, b) {
+    return a * b
+}
+
+Calculator.prototype.divide = function(a, b) {
+    if(b !== 0) {
+        return a / b;
+    } else {
+        return 'Nie można dzielić przez zero'
+    }
+}
+
+Calculator.prototype.exponential = function(a, b) {
+    if(b === 0) {
+        return 1;            
+    } else {
+        i = 1;
+        result = 1;
+        while(i<=Math.abs(b)) {
+            result *= a;
+            i++;
+        }                    
+        if(b < 0) {
+            result = 1/result; 
+        }  
+        
+        return result
+    }     
+}
+
+Calculator.prototype.getResult = function(num1, num2, action) {
     // 1. zamień wartości przekazane przez parametr na typ number
     num1 = Number(num1);
     num2 = Number(num2);
+    operation = this.actions[action]
     // 2. sprawdź czy są one poprawne
-    // 3. jeśli tak to wykonaj działanie i zapisz jego rezultat
-    // 4. dodaj do historii operacji to działanie w formie: 1 + 1 = 2    
     if(!isNaN(num1) && !isNaN(num2)) {
-        let result;
-        switch(action) {
-            case '+':
-                result = num1 + num2;
-                break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/':
-                if(num2 !== 0) {
-                    result = num1 / num2;
-                } else {
-                    result = 'Nie można dzielić przez zero'
-                }
-                break;
-            case '^':
-                if(num2 === 0) {
-                    result = 1;            
-                } else {
-                    i = 1;
-                    result = 1;
-                    while(i<=Math.abs(num2)) {
-                        result *= num1;
-                        i++;
-                    }                    
-                    if(num2 < 0) {
-                        result = 1/result; 
-                    }                    
-                }     
-                break;  
-        } 
-            this.history.push(`${num1} ${action} ${num2} = ${result}`);
+        // 3. jeśli tak to wykonaj działanie i zapisz jego rezultat
+        const result = operation(num1, num2)
+        // 4. dodaj do historii operacji to działanie w formie: 1 + 1 = 2    
+        this.history.push(`${num1} ${action} ${num2} = ${result}`)
     } else {
-        alert('Podałeś błędne dane. Próbuj jeszcze raz!');
+    alert('Podałeś błędne dane. Próbuj jeszcze raz!');
     }
 }
 
@@ -73,7 +85,7 @@ do {
         number1 = prompt('Podaj liczbę nr 1');
         number2 = prompt('Podaj liczbę nr 2');
 
-        calc.operation(number1, number2, action);        
+        calc.getResult(number1, number2, action);        
     } else {
         alert('Podałeś błędne dane. Odśwież stronę i próbuj ponownie!');
     }
