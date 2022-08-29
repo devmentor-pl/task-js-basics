40de51315f896d7cec0603efb44019756bf806d4
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2022-08-29 16:26:44 +0200

    Refactor code

--- js/app.js
+++ js/app.js
@@ -10,75 +10,55 @@ Calculator.prototype.isCorrectAction = function(action) {
 Calculator.prototype.getHistoryAsString = function() {
     return this.history.join('\n');
 }
-Calculator.prototype.helper = function(op, num1, num2){
-    const n1 = parseFloat(num1);
-    const n2 = parseFloat(num2);
-
-    const operations = {
-        '+': function(n1, n2){
-            const result = n1 + n2;
-            return result;
-        },
-        '-': function(n1, n2){
-            const result = n1 - n2;
-            return result;
-        },
-        '*': function(n1, n2){
-            const result = n1 * n2;
-            return result;
-        },
-        '/': function(n1, n2){
-            const result = n1 / n2;
-            return result.toFixed(3);
-        },
-        '^': function(n1, n2){
-                let result;
-                let exponent;
-                if(n2<0){
-                    exponent = -n2; 
-                }else{
-                    exponent = n2;
-                }
-                if(exponent == 0 || exponent == -0){
-                    result = 1;
-                }else if(exponent == 1){
-                    result = n1;
-                }else if(exponent > 1){
-                    result = n1;
-                    for(let i = 2; i <= exponent ; i++){
-                        result *= n1;
-                    }
-                }
-                if(n2<0){
-                    result = 1/result;
-                }
-                return result
-        },
-        
-    }
-    if(typeof n1 && typeof n2 === 'number'){
-        calc.history.push(n1 + ' ' + op + ' ' + n2 + ' = ' + operations[op](n1, n2));
+Calculator.prototype.isCorrectNumber = function(n1, n2){
+    if(typeof n1 === 'number' && typeof n2 === 'number' 
+    && typeof n1 !== NaN && typeof n2 !== NaN){
+        return true;
     }
 }
 Calculator.prototype.add = function(num1, num2) {
-    Calculator.prototype.helper('+', num1, num2);
+   return num1 + num2;
 }
 Calculator.prototype.sub = function(num1, num2){
-    Calculator.prototype.helper('-', num1, num2);
+    return num1 - num2;
 }
 Calculator.prototype.multi = function(num1, num2){
-    Calculator.prototype.helper('*', num1, num2);
-    }
+    return num1 * num2;
+}
 Calculator.prototype.div = function(num1, num2){
-    Calculator.prototype.helper('/', num1, num2);
-        
+    const result = num1 / num2;
+    if(Number.isSafeInteger(result)){
+        return result;
+    }else{
+        return result.toFixed(3);
     }
+}
 Calculator.prototype.pow = function(num1, num2){
-    Calculator.prototype.helper('^', num1, num2);
+    let result;
+    let exponent;
+    if(num2 < 0){
+        exponent = -num2; 
+    }else{
+        exponent = num2;
+    }
+    if(exponent == 0 || exponent == -0){
+        result = 1;
+    }else if(exponent == 1){
+        result = num1;
+    }else if(exponent > 1){
+        result = num1;
+        for(let i = 2; i <= exponent ; i++){
+            result *= num1;
+        }
+    }
+    if(num2<0){
+        result = 1/result;
+    }
+    return result;
 }
 
 const calc = new Calculator();
-let action, promptContent, isCorrectAction, number1, number2;
+let action, promptContent, isCorrectAction, isCorrectNumber, number1, number2;
 do { 
     promptContent = 'Podaj jaką operację chcesz wykonać (+, -, *, /, ^) i potwierdź. \n'; // \n - znak nowej linii
     promptContent += 'Jeśli chcesz zrezygnować wciśnij Anuluj. \n';
@@ -89,19 +69,22 @@ do {
     if(isCorrectAction) {
         number1 = prompt('Podaj liczbę nr 1');
         number2 = prompt('Podaj liczbę nr 2');
-
-        if(action === '+') {
-            calc.add(number1, number2);
-        }else if(action === '-'){
-            calc.sub(number1, number2);
-        }else if(action === '*'){
-            calc.multi(number1, number2);
-        }else if(action === '/'){
-            calc.div(number1, number2);
-        }else{
-            calc.pow(number1, number2);
+        const n1 = parseInt(number1);
+        const n2 = parseInt(number2);
+        isCorrectNumber = calc.isCorrectNumber(n1, n2);
+        
+        if(isCorrectNumber){
+            const operations = {
+                '+': calc.add,
+                '-': calc.sub,
+                '*': calc.multi,
+                '/': calc.div,
+                '^': calc.pow, 
+            };
+            const result = operations[action](n1, n2);
+            
+            calc.history.push(n1 + ' ' + action + ' ' + n2 + ' ' + '=' + ' ' + result);
         }
-
     }
     
 } while(calc.isCorrectAction(action));
