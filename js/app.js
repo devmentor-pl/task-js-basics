9c167b5f65f598459fb1fb6922d14a3d52231bc3
Jacek Zielinski <gandi34@wp.pl>
2022-09-14 14:38:46 +0200

    task js basics

--- js/app.js
+++ js/app.js
@@ -13,7 +13,10 @@ Calculator.prototype.getHistoryAsString = function() {
 
 Calculator.prototype.add = function(num1, num2) {
     // 1. zamień wartości przekazane przez parametr na typ number
+    const intNum1 = parseInt (num1);
+    const intNum2 = parseInt (num2);
     // 2. sprawdź czy są one poprawne
+    console.log(num1,num2);
     // 3. jeśli tak to wykonaj działanie i zapisz jego resultat
     // 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
 }
