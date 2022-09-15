dd609d3262a785a59e1aff2a03ca41d9138b9959
Jacek Zielinski <gandi34@wp.pl>
2022-09-15 13:17:10 +0200

    Calculator prototype

--- js/app.js
+++ js/app.js
@@ -13,21 +13,40 @@ Calculator.prototype.getHistoryAsString = function() {
 
 Calculator.prototype.add = function(num1, num2) {
     let result = 0;
-    // 1. zamień wartości przekazane przez parametr na typ number
-    const intNum1 = parseInt (num1);
-    const intNum2 = parseInt (num2);
-    
-    // 2. sprawdź czy są one poprawne
-    if (typeof intNum1 ==='number' && typeof intNum2 ==='number') {}
-
-    // 3. jeśli tak to wykonaj działanie i zapisz jego resultat
-    result = intNum1 + intNum2;
-    alert(result);
-    
-    
-    // 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
-    this.history.push (intNum1 + ' + ' + intNum2 + " = " + result);
+    result = num1 + num2;
+    this.history.push (`${num1} + ${num2} = ${result}`);
 };
+
+
+
+Calculator.prototype.sub = function(num1 ,num2) {
+    let result = 0;
+    result = num1 - num2;
+    this.history.push (`${num1} - ${num2} = ${result}`);
+};
+
+
+Calculator.prototype.multi = function(num1 ,num2) {
+    let result = 0;
+    result = num1 * num2;
+    this.history.push (`${num1} * ${num2} = ${result}`);
+};
+
+
+Calculator.prototype.div = function(num1 ,num2) {
+    let result = 0;
+    result = num1 / num2;
+    this.history.push (`${num1} / ${num2} = ${result}`);
+};
+
+
+Calculator.prototype.expo = function(num1 ,num2) {
+    let result = 0;
+    result = num1 ^ num2;
+    this.history.push (`${num1} * ${num2} = ${result}`);
+};
+
+
 
 const calc = new Calculator();
 let action, promptContent, isCorrectAction, number1, number2;
@@ -39,12 +58,34 @@ do {
     action = prompt(promptContent);
     isCorrectAction = calc.isCorrectAction(action);
     if(isCorrectAction) {
-        number1 = prompt('Podaj liczbę nr 1');
-        number2 = prompt('Podaj liczbę nr 2');
+        number1 = parseInt(prompt('Podaj liczbę nr 1'));
+        number2 = parseInt(prompt('Podaj liczbę nr 2'));
+        /*const intNum1 = parseInt (num1);
+        const intNum2 = parseInt (num2);*/
+    
+        
+    if (!Number.isNaN(number1) && !Number.isNaN(number2)){
 
         if(action === '+') {
-            calc.add(number1, number2);
+                calc.add(number1, number2);
+        }
+
+        if(action ==='-' ) {
+                calc.sub(number1 , number2);
+        }
+
+        if(action ==='*' ) {
+                calc.multi(number1 , number2);
+        }
+
+        if(action ==='/' ) {
+                calc.div(number1 , number2);
+        }
+
+        if(action ==='^' ) {
+                calc.expo(number1 , number2);
         }
     }
-    
+}
+
 } while(calc.isCorrectAction(action));
