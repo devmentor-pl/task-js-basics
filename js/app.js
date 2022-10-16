986da61d784101c256ce4439112c561d2d1a1d1f
Norbert sasak <norbert.sasak@gmail.com>
2022-10-17 01:16:01 +0200

    task done

--- js/app.js
+++ js/app.js
@@ -2,78 +2,73 @@ function Calculator() {
     this.actions = ['+', '-', '*', '/', '^'];
     this.history = [];
 }
-
 Calculator.prototype.isCorrectAction = function (action) {
     return this.actions.includes(action);
 }
-
 Calculator.prototype.getHistoryAsString = function () {
     return this.history.join('\n');
 }
 
 
-
-
+Calculator.prototype.checkNumber = function (num1, num2) {
+    let number1 = Number(num1);
+    let number2 = Number(num2);
+    if ((number1 && number2)) {
+        return true;
+    } else {
+        alert("Spróbuj ponownie. Podane wyrażenia to nie liczby")
+    }
+}
 
 Calculator.prototype.add = function (num1, num2) {
-    this.result1 = parseInt(num1);
-    this.result2 = parseInt(num2);
-    const a = 2;
-    if (typeof this.result1 === typeof a && typeof this.result2 === typeof a) {
-        this.result = this.result1 + this.result2;
-        const resultFinal = `${this.result1} + ${this.result2} = ${this.result}`;
-        this.history.push(resultFinal);
-    }
-
     // 1. zamień wartości przekazane przez parametr na typ number
     // 2. sprawdź czy są one poprawne
-    // 3. jeśli tak to wykonaj działanie i zapisz jego resultat
-    // 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
-}
 
+    this.checkNumber(num1, num2); // CZY TAK MOGĘ DODAĆ INNA METODE STWORZONĄ W PROTO DO INNEJ METODY STWORZONEJ W PROTO?
+
+    // 3. jeśli tak to wykonaj działanie i zapisz jego resultat
+    let result = num1 + num2;
+    // 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
+    this.history.push(`${num1} + ${num2} = ${result}`);
+}
 
 Calculator.prototype.sub = function (num1, num2) {
-    this.result1 = parseInt(num1);
-    this.result2 = parseInt(num2);
-    const a = 2;
-    if (typeof this.result1 === typeof a && typeof this.result2 === typeof a) {
-        this.result = this.result1 - this.result2;
-        const resultFinal = `${this.result1} - ${this.result2} = ${this.result}`;
-        this.history.push(resultFinal);
-    }
+    this.checkNumber(num1, num2);
+    let result = num1 - num2;
+    this.history.push(`${num1} - ${num2} = ${result}`);
 }
+// Calculator.prototype.sub = function (num1, num2) {
+//     this.result1 = parseInt(num1);
+//     this.result2 = parseInt(num2);
+//     const a = 2;
+//     if (typeof this.result1 && typeof this.result2 === typeof a) {
+//         this.result = this.result1 - this.result2;
+//         this.history.push(`${this.result1} - ${this.result2} = ${this.result}`);
+//     }
+// }
+//  Mógłbyś mi powiedzieć czy taki typ zapisu WYŻEJ jest zły??
+
+
 Calculator.prototype.multi = function (num1, num2) {
-    this.result1 = parseInt(num1);
-    this.result2 = parseInt(num2);
-    const a = 2;
-    if (typeof this.result1 === typeof a && typeof this.result2 === typeof a) {
-        this.result = this.result1 * this.result2;
-        const resultFinal = `${this.result1} * ${this.result2} = ${this.result}`;
-        this.history.push(resultFinal);
-    }
+    this.checkNumber(num1, num2);
+    let result = num1 * num2;
+    this.history.push(`${num1} * ${num2} = ${result}`);
+
 }
 Calculator.prototype.div = function (num1, num2) {
-    this.result1 = parseInt(num1);
-    this.result2 = parseInt(num2);
-    const a = 2;
-    if (typeof this.result1 === typeof a && typeof this.result2 === typeof a) {
-        this.result = this.result1 / this.result2;
-        const resultFinal = `${this.result1} / ${this.result2} = ${this.result}`;
-        this.history.push(resultFinal);
-    }
+    this.checkNumber(num1, num2);
+    let result = num1 / num2;
+    this.history.push(`${num1} / ${num2} = ${result}`);
 }
-Calculator.prototype.exponentation = function (num1, num2) {
-    this.result1 = parseInt(num1);
-    this.result2 = parseInt(num2);
-    const a = 2;
-    if (typeof this.result1 === typeof a && typeof this.result2 === typeof a) {
-        let exp = 1;
-        for (let i = 1; i <= this.result2; i++) {
-            exp *= this.result1;
-        }
-        const resultFinal = `${this.result1} ^ ${this.result2} = ${exp}`;
-        this.history.push(resultFinal);
+
+
+Calculator.prototype.exp = function (num1, num2) {
+    this.checkNumber(num1, num2);
+    let exp = 1;
+    for (let i = 1; i <= num2; i++) {
+        exp *= num1;
     }
+    this.history.push(`${num1} ^ ${num2} = ${exp}`);
 }
 
 
