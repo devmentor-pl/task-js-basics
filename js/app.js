27fb942e6ce98b4c8c076ecfc2c680a2457955f4
Michal Lata <maxpoz3212@gmail.com>
2022-12-28 14:12:58 +0100

    Add calculator program

--- js/app.js
+++ js/app.js
@@ -3,37 +3,120 @@ function Calculator() {
     this.history = [];
 }
 
-Calculator.prototype.isCorrectAction = function(action) {
+Calculator.prototype.isCorrectAction = function (action) {
     return this.actions.includes(action);
 }
 
-Calculator.prototype.getHistoryAsString = function() {
+Calculator.prototype.getHistoryAsString = function () {
     return this.history.join('\n');
 }
 
-Calculator.prototype.add = function(num1, num2) {
+Calculator.prototype.add = function (num1, num2) {
     // 1. zamień wartości przekazane przez parametr na typ number
     // 2. sprawdź czy są one poprawne
     // 3. jeśli tak to wykonaj działanie i zapisz jego resultat
     // 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
+
+    let result = (num1 + num2);
+    result = `${num1} + ${num2} = ${result}`;
+    this.history.push(result);
+
+    return result;
+}
+
+Calculator.prototype.substract = function (num1, num2) {
+
+    let result = (num1 - num2);
+    result = `${num1} - ${num2} = ${result}`;
+    this.history.push(result);
+
+    return result;
+}
+
+Calculator.prototype.multiply = function (num1, num2) {
+
+    let result = (num1 * num2);
+    result = `${num1} * ${num2} = ${result}`;
+    this.history.push(result);
+
+    return result;
+}
+
+Calculator.prototype.divide = function (num1, num2) {
+
+    let result = (num1 / num2);
+    result = `${num1} / ${num2} = ${result}`;
+    this.history.push(result);
+
+    return result;
+}
+
+Calculator.prototype.power = function (a, n) {
+    let result = 1;
+    let output = '';
+
+    if (a === 0 || a === 1) {
+        output = `${a} podniesione do każdej potęgi daje ${a}`;
+    } else if (n === 0) {
+        output = 'Każda liczba podniesiona do potęgi 0 daje 1';
+    } else if (n < 0) {
+        n = Math.abs(n);
+        let j = 0;
+        while (j < n) {
+            result = result / a;
+            j++;
+        }
+        output = `${a} podniesione do -${n} daje odwrotność tej liczby, czyli ${result}`;
+
+    } else {
+        let i = 0;
+        while (i < n) {
+            result = result * a;
+
+            if (i > 0) {
+                output += ' * ';
+            }
+
+            output += a;
+            i++;
+        }
+        output += ' = ' + result;
+    }
+
+    this.history.push(output);
+
+    return output;
 }
 
 const calc = new Calculator();
 let action, promptContent, isCorrectAction, number1, number2;
-do { 
+do {
     promptContent = 'Podaj jaką operację chcesz wykonać (+, -, *, /, ^) i potwierdź. \n'; // \n - znak nowej linii
     promptContent += 'Jeśli chcesz zrezygnować wciśnij Anuluj. \n';
     promptContent += 'Lista poprzednich operacji: \n' + calc.getHistoryAsString();
 
     action = prompt(promptContent);
     isCorrectAction = calc.isCorrectAction(action);
-    if(isCorrectAction) {
-        number1 = prompt('Podaj liczbę nr 1');
-        number2 = prompt('Podaj liczbę nr 2');
+    if (isCorrectAction) {
+        number1 = Number(prompt('Podaj liczbę nr 1'));
+        number2 = Number(prompt('Podaj liczbę nr 2'));
+        // number1 = Number(2);
+        // number2 = Number(3);
 
-        if(action === '+') {
-            calc.add(number1, number2);
-        }
+        if (isNaN(number1) || isNaN(number2)) {
+            alert('To nie wygląda jak liczba...');
+        } else {
+            if (action === '+') {
+                calc.add(number1, number2);
+            } else if (action === '-') {
+                calc.substract(number1, number2);
+            } else if (action === '*') {
+                calc.multiply(number1, number2);
+            } else if (action === '/') {
+                calc.divide(number1, number2);
+            } else if (action === '^') {
+                calc.power(number1, number2);
+            }
+        };
     }
-    
-} while(calc.isCorrectAction(action));
+} while (calc.isCorrectAction(action));
