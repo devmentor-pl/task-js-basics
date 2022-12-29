d460674851572e9645aed0259495483c38f728db
Michal Lata <maxpoz3212@gmail.com>
2022-12-29 13:38:59 +0100

    Add object prototypes refactoring

--- js/app.js
+++ js/app.js
@@ -33,35 +33,19 @@ Calculator.prototype.addHistory = function (result) {
 }
 
 Calculator.prototype.add = function (num1, num2) {
-
-    let result = (num1 + num2);
-    result = `${num1} + ${num2} = ${result} `;
-
-    return result;
+    return `${num1} + ${num2} = ${num1 + num2}`;
 }
 
 Calculator.prototype.subtract = function (num1, num2) {
-
-    let result = (num1 - num2);
-    result = `${num1} - ${num2} = ${result} `;
-
-    return result;
+    return `${num1} - ${num2} = ${num1 - num2}`;
 }
 
 Calculator.prototype.multiply = function (num1, num2) {
-
-    let result = (num1 * num2);
-    result = `${num1} * ${num2} = ${result} `;
-
-    return result;
+    return result = `${num1} * ${num2} = ${num1 * num2}`;
 }
 
 Calculator.prototype.divide = function (num1, num2) {
-
-    let result = (num1 / num2);
-    result = `${num1} / ${num2} = ${result}`;
-
-    return result;
+    return `${num1} / ${num2} = ${num1 / num2}`;
 }
 
 Calculator.prototype.power = function (a, n) {
