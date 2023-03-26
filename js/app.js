231932fa88e96ed0f1cc7a3c84609926419daf76
Paweł Niedużak <93704910+czarsbenemoriendi@users.noreply.github.com>
2023-03-26 16:16:19 +0200

    refactor: rebuild app functions

--- js/app.js
+++ js/app.js
@@ -1,8 +1,33 @@
 function Calculator() {
-	this.actions = ['+', '-', '*', '/', '^'];
+	this.actions = ['+', '-', '*', '/', '^', '%'];
 	this.history = [];
 }
-
+const addition = (firstNumber, secondNumber) => {
+	return firstNumber + secondNumber;
+};
+const subtraction = (firstNumber, secondNumber) => {
+	return firstNumber - secondNumber;
+};
+const multiplication = (firstNumber, secondNumber) => {
+	return firstNumber * secondNumber;
+};
+const exponentiation = (firstNumber, secondNumber) => {
+	return firstNumber ** secondNumber;
+};
+const division = (firstNumber, secondNumber) => {
+	return firstNumber / secondNumber;
+};
+const modulus = (firstNumber, secondNumber) => {
+	return firstNumber % secondNumber;
+};
+const operations = {
+	'+': addition,
+	'-': subtraction,
+	'*': multiplication,
+	'^': exponentiation,
+	'/': division,
+	'%': modulus,
+};
 Calculator.prototype.isCorrectAction = function (action) {
 	return this.actions.includes(action);
 };
@@ -10,54 +35,14 @@ Calculator.prototype.isCorrectAction = function (action) {
 Calculator.prototype.getHistoryAsString = function () {
 	return this.history.join('\n');
 };
-
-Calculator.prototype.add = function (num1, num2, action) {
-	return calculate(num1, num2, action);
-};
-Calculator.prototype.subtraction = function (num1, num2, action) {
-	return calculate(num1, num2, action);
-};
-Calculator.prototype.multiply = function (num1, num2, action) {
-	return calculate(num1, num2, action);
-};
-Calculator.prototype.power = function (num1, num2, action) {
-	return calculate(num1, num2, action);
-};
-Calculator.prototype.division = function (num1, num2, action) {
-	return calculate(num1, num2, action);
-};
-const calculate = (number1, number2, action) => {
-	let result = 1;
-	num1 = parseFloat(number1);
-	num2 = parseFloat(number2);
-
-	switch (action) {
-		case '+':
-			result = num1 + num2;
-			break;
-		case '-':
-			result = num1 - num2;
-			break;
-		case '*':
-			result = num1 * num2;
-			break;
-		case '/':
-			result = num1 / num2;
-			break;
-		case '^':
-			for (let i = 0; i < num2; i++) result *= num1;
-			break;
-		default:
-			result = 'Invalid operator';
-	}
-	if (typeof result === 'string') return;
-	operation = `${num1} ${action} ${num2} = ${result}`;
+Calculator.prototype.createHistory = function (num1, num2, action, result) {
+	const operation = `${num1} ${action} ${num2} = ${result}`;
 	calc.history.push(operation);
-	return;
+	return result;
 };
+
 const calc = new Calculator();
 let action, promptContent, isCorrectAction, number1, number2, result;
-
 do {
 	promptContent = 'Podaj jaką operację chcesz wykonać (+, -, *, /, ^) i potwierdź. \n';
 	promptContent += 'Jeśli chcesz zrezygnować wciśnij Anuluj. \n';
@@ -67,21 +52,17 @@ do {
 	isCorrectAction = calc.isCorrectAction(action);
 	if (isCorrectAction) {
 		const regExp = new RegExp(/[^0-9]/);
+		const createResult = operations[action];
+
 		number1 = prompt('Podaj liczbę nr 1');
 		number2 = prompt('Podaj liczbę nr 2');
 
 		if (!regExp.test(number1) && !regExp.test(number2)) {
-			if (action === '+') {
-				calc.add(number1, number2, action);
-			} else if (action === '-') {
-				calc.subtraction(number1, number2, action);
-			} else if (action === '*') {
-				calc.multiply(number1, number2, action);
-			} else if (action === '^') {
-				calc.power(number1, number2, action);
-			} else if (action === '/') {
-				calc.division(number1, number2, action);
-			}
+			num1 = parseFloat(number1);
+			num2 = parseFloat(number2);
+
+			const result = createResult(num1, num2);
+			calc.createHistory(num1, num2, action, result);
 		}
 	}
 } while (calc.isCorrectAction(action));
