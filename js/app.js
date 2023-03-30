6a05a68315d8714d81d003cb90abe097a4b0d84d
Mikołaj Krzywacz <mikolaj.krzywacz@gmail.com>
2023-03-30 19:41:05 +0200

    add all math operations

--- js/app.js
+++ js/app.js
@@ -1,39 +1,108 @@
 function Calculator() {
-    this.actions = ['+', '-', '*', '/', '^'];
-    this.history = [];
+  this.actions = ["+", "-", "*", "/", "^"];
+  this.history = [];
 }
 
-Calculator.prototype.isCorrectAction = function(action) {
-    return this.actions.includes(action);
+function isInputANumber(num) {
+  return !isNaN(Number(num)) && Number(num) !== undefined;
 }
 
-Calculator.prototype.getHistoryAsString = function() {
-    return this.history.join('\n');
-}
+Calculator.prototype.isCorrectAction = function (action) {
+  return this.actions.includes(action);
+};
 
-Calculator.prototype.add = function(num1, num2) {
-    // 1. zamień wartości przekazane przez parametr na typ number
-    // 2. sprawdź czy są one poprawne
-    // 3. jeśli tak to wykonaj działanie i zapisz jego resultat
-    // 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
-}
+Calculator.prototype.getHistoryAsString = function () {
+  return this.history.join("\n");
+};
+
+Calculator.prototype.add = function (num1, num2) {
+  if (isInputANumber(num1) && isInputANumber(num2)) {
+    num1 = Number(num1);
+    num2 = Number(num2);
+    const equationString = `${num1} + ${num2} = ${num1 + num2}`;
+    this.history.push(equationString);
+    return num1 + num2;
+  }
+  // 1. zamień wartości przekazane przez parametr na typ number
+  // 2. sprawdź czy są one poprawne
+  // 3. jeśli tak to wykonaj działanie i zapisz jego resultat
+  // 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
+};
+
+Calculator.prototype.subtract = function (num1, num2) {
+  if (isInputANumber(num1) && isInputANumber(num2)) {
+    num1 = Number(num1);
+    num2 = Number(num2);
+    const equationString = `${num1} - ${num2} = ${num1 - num2}`;
+    this.history.push(equationString);
+    return num1 - num2;
+  }
+};
+
+Calculator.prototype.multiply = function (num1, num2) {
+  if (isInputANumber(num1) && isInputANumber(num2)) {
+    num1 = Number(num1);
+    num2 = Number(num2);
+    const equationString = `${num1} * ${num2} = ${num1 * num2}`;
+    this.history.push(equationString);
+    return num1 * num2;
+  }
+};
+
+Calculator.prototype.divide = function (num1, num2) {
+  if (isInputANumber(num1) && isInputANumber(num2)) {
+    num1 = Number(num1);
+    num2 = Number(num2);
+    const equationString = `${num1} / ${num2} = ${num1 / num2}`;
+    this.history.push(equationString);
+    return num1 / num2;
+  }
+};
+
+Calculator.prototype.power = function (num1, num2) {
+  if (isInputANumber(num1) && isInputANumber(num2)) {
+    num1 = Number(num1);
+    num2 = Number(num2);
+    let exponentiationResult = 1;
+    for (let i = 0; i < num2; i++) {
+      exponentiationResult *= num1;
+    }
+
+    const equationString = `${num1} ^ ${num2} = ${exponentiationResult}`;
+    this.history.push(equationString);
+    return exponentiationResult;
+  }
+};
 
 const calc = new Calculator();
 let action, promptContent, isCorrectAction, number1, number2;
-do { 
-    promptContent = 'Podaj jaką operację chcesz wykonać (+, -, *, /, ^) i potwierdź. \n'; // \n - znak nowej linii
-    promptContent += 'Jeśli chcesz zrezygnować wciśnij Anuluj. \n';
-    promptContent += 'Lista poprzednich operacji: \n' + calc.getHistoryAsString();
+do {
+  promptContent =
+    "Podaj jaką operację chcesz wykonać (+, -, *, /, ^) i potwierdź. \n"; // \n - znak nowej linii
+  promptContent += "Jeśli chcesz zrezygnować wciśnij Anuluj. \n";
+  promptContent += "Lista poprzednich operacji: \n" + calc.getHistoryAsString();
 
-    action = prompt(promptContent);
-    isCorrectAction = calc.isCorrectAction(action);
-    if(isCorrectAction) {
-        number1 = prompt('Podaj liczbę nr 1');
-        number2 = prompt('Podaj liczbę nr 2');
+  action = prompt(promptContent);
+  isCorrectAction = calc.isCorrectAction(action);
+  if (isCorrectAction && action === "^") {
+    number1 = prompt("Podaj podstawę");
+    number2 = prompt("Podaj potęgę");
+    calc.power(number1, number2);
+  } else if (isCorrectAction) {
+    number1 = prompt("Podaj liczbę nr 1");
+    number2 = prompt("Podaj liczbę nr 2");
 
-        if(action === '+') {
-            calc.add(number1, number2);
-        }
+    if (action === "+") {
+      calc.add(number1, number2);
     }
-    
-} while(calc.isCorrectAction(action));
+    if (action === "-") {
+      calc.subtract(number1, number2);
+    }
+    if (action === "*") {
+      calc.multiply(number1, number2);
+    }
+    if (action === "/") {
+      calc.divide(number1, number2);
+    }
+  }
+} while (calc.isCorrectAction(action));
