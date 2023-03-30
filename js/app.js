7af5ccebfe74e373f7c27c78be3bdfd5873bc0a4
Andrzej Przybylski <andrzej.prokocim@gmail.com>
2023-03-30 22:50:53 +0200

    Add calculator functions

--- js/app.js
+++ js/app.js
@@ -11,15 +11,37 @@ Calculator.prototype.getHistoryAsString = function() {
     return this.history.join('\n');
 }
 
+function isUserDataNaN(num1, num2) {
+    return isNaN(num1) || isNaN(num2) ? true : false
+}
+
 Calculator.prototype.add = function(num1, num2) {
-    // 1. zamień wartości przekazane przez parametr na typ number
-    // 2. sprawdź czy są one poprawne
-    // 3. jeśli tak to wykonaj działanie i zapisz jego resultat
-    // 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
+    this.history.push(`${num1} + ${num2} = ${num1 + num2}`)
+}
+
+Calculator.prototype.odd = function(num1, num2) {
+    this.history.push(`${num1} - ${num2} = ${num1 - num2}`)
+}
+
+Calculator.prototype.multiply = function(num1, num2) {
+    this.history.push(`${num1} * ${num2} = ${num1 * num2}`)
+}
+
+Calculator.prototype.divide = function(num1, num2) {
+    this.history.push(`${num1} / ${num2} = ${num1 / num2}`)
+}
+
+Calculator.prototype.pow = function(num1, num2) {
+    let resultant = num2 !== 0 ? num1 : 0;
+    for(let i = 1; i < num2; i++) {
+        resultant = resultant * num1
+    }
+
+    this.history.push(`${num1} ^ ${num2} = ${resultant}`)
 }
 
 const calc = new Calculator();
-let action, promptContent, isCorrectAction, number1, number2;
+let action, promptContent, isCorrectAction, userNumber1, userNumber2;
 do { 
     promptContent = 'Podaj jaką operację chcesz wykonać (+, -, *, /, ^) i potwierdź. \n'; // \n - znak nowej linii
     promptContent += 'Jeśli chcesz zrezygnować wciśnij Anuluj. \n';
@@ -28,11 +50,32 @@ do {
     action = prompt(promptContent);
     isCorrectAction = calc.isCorrectAction(action);
     if(isCorrectAction) {
-        number1 = prompt('Podaj liczbę nr 1');
-        number2 = prompt('Podaj liczbę nr 2');
+        userNumber1 = prompt('Podaj liczbę nr 1');
+        userNumber2 = prompt('Podaj liczbę nr 2');
 
-        if(action === '+') {
-            calc.add(number1, number2);
+        if (userNumber1 !== null && userNumber2 !== null) {
+            const number1 = parseFloat(userNumber1);
+            const number2 = parseFloat(userNumber2);
+
+            switch (action !== "" || !isUserDataNaN(number1, number2)) {
+                case action === "+": 
+                    calc.add(number1, number2);
+                    break;
+                case action === "-": 
+                    calc.odd(number1, number2);
+                    break;
+                case action === "*": 
+                    calc.multiply(number1, number2);
+                    break;
+                case action === "/": 
+                    calc.divide(number1, number2);
+                    break;
+                case action === "^": 
+                    calc.pow(number1, number2);
+                    break;
+                default:
+                    break;
+            }
         }
     }
     
