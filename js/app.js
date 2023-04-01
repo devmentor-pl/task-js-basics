affd77e2a45d2f0390e46ab44aee9b74bc8766a8
Jakub Kasperkowicz <jakub.kasperkowicz@gmail.com>
2023-04-02 01:29:01 +0200

    almost done it works but small improvments can be done

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 function Calculator() {
     this.actions = ['+', '-', '*', '/', '^'];
     this.history = [];
+ 
 }
 
 Calculator.prototype.isCorrectAction = function(action) {
@@ -11,67 +12,120 @@ Calculator.prototype.getHistoryAsString = function() {
     return this.history.join('\n');
 }
 
-Calculator.prototype.sum = function(num1, num2) {
+// Calculator.prototype.sum = function(num1, num2) {
 
-    const result = Number(num1) + Number(num2)
-        if(isNaN(result) === true){
-            this.history.push ('Wprowadzono błędnie dane')
-        }else{
-            this.history.push ( (num1) + " + " + (num2) + ' = ' + (result))
-        return result
-        }
-}
-Calculator.prototype.subtract = function(num1, num2) {
+//     const result = Number(num1) + Number(num2)
+//         if(isNaN(result) === true){
+//             this.history.push ('Wprowadzono błędnie dane')
+//         }else{
+//             this.history.push ( (num1) + " + " + (num2) + ' = ' + (result))
+//         return result
+//         }
+// }
+// Calculator.prototype.subtract = function(num1, num2) {
 
-    const result = Number(num1) - Number(num2)
-        if(isNaN(result) === true){
-            this.history.push ('Wprowadzono błędnie dane')
-        }else{
-            this.history.push ( (num1) + " - " + (num2) + ' = ' + (result))
-        return result
-        }
-}
-Calculator.prototype.multiply = function(num1, num2) {
+//     const result = Number(num1) - Number(num2)
+//         if(isNaN(result) === true){
+//             this.history.push ('Wprowadzono błędnie dane')
+//         }else{
+//             this.history.push ( (num1) + " - " + (num2) + ' = ' + (result))
+//         return result
+//         }
+// }
+// Calculator.prototype.multiply = function(num1, num2) {
 
-    const result = Number(num1) * Number(num2)
-        if(isNaN(result) === true){
-            this.history.push ('Wprowadzono błędnie dane')
-        }else{
-            this.history.push ( (num1) + " * " + (num2) + ' = ' + (result))
-        return result
-        }
-}
+//     const result = Number(num1) * Number(num2)
+//         if(isNaN(result) === true){
+//             this.history.push ('Wprowadzono błędnie dane')
+//         }else{
+//             this.history.push ( (num1) + " * " + (num2) + ' = ' + (result))
+//         return result
+//         }
+// }
+
 Calculator.prototype.divide = function(num1, num2) {
-    if(num2 === 0){
-        console.log("you can't divide by 0")
+    if((num1) === NaN){
+        this.history.push ('Wprowadzono błędnie dane')
+    }else if(num2 === 0){
+            this.history.push ('Nie dzielimy przez 0')
     }else{
-    const result = Number(num1) / Number(num2)
-        if(isNaN(result) === true){
-            this.history.push ('Wprowadzono błędnie dane')
-        }else if(isFinite(result) === false){
-            this.history.push ('Nie dzielimy przez zero!')
-        }else{
-            this.history.push ( (num1) + " / " + (num2) + ' = ' + (result))
-        return result
+            result = num1 / num2
+            return result
         }
-    }
 }
-Calculator.prototype.power = function(num1, num2) {
+Calculator.prototype.exponentiation = function(num1, num2) {
 
         if ((num2) === 0) {
-            this.history.push('1')
-          return 1;
-          
+            // this.history.push('1')
+          return 1;   
         }
         let result = 1;
         for (let i = 0; i < num2; i++) {
           result = result * num1;
         }
-        this.history.push ( (num1) + " ^ " + (num2) + ' = ' + (result))
         return result;
 }
-      
+Calculator.prototype.changeToNumbers = function(num1, num2){
+    const numbers = [Number(num1), Number(num2)]
+    console.log(numbers)
+    return numbers
+}
+Calculator.prototype.numbersAreCorrect = function(numbers){
+    const containsNaN = numbers.some(Number.isNaN) //true if we check and find NaN
+    if(containsNaN){
+        this.history.push ('Wprowadzono błędne dane') //push some error
+    }else {
+        console.log(containsNaN) //checks if the numbers in the array are not NaN: if true - are; if false - aren't 
+        return containsNaN 
+    }
+}
+Calculator.prototype.getResult = function(num1, num2){
+    let result;
+    switch (action) {
+        case "+":
+            result = num1 + num2
+            // result = Number(num1) + Number(num2)
+            break
+        case "-":
+            result = num1 - num2
+            break
+        case "*":
+            result = num1 * num2
+            break
+        case "/":
+            result = this.divide(num1, num2)
+            break
+        case "^":
+            result = this.exponentiation(num1, num2)
+            break
+            // default:
+            //     this.history.push ('Wprowadzono błędne dane')
+        }
+        return result
+    }  
+ 
 
+Calculator.prototype.calculate = function (num1, num2){
+
+    //1. Zamiana wprowadzonych danych na liczby
+    console.log(num1, num2)
+    const numbers = this.changeToNumbers(num1, num2)
+    //2. Sprawdzenie czy wprowadzone dane są poprawne
+    const isCorrect = this.numbersAreCorrect(numbers)
+    //3. Wykonanie obliczeń pod warunkiem poprawności danych+
+    let value
+
+    if(!isCorrect){
+
+        value = this.getResult(numbers[0], numbers[1])
+        console.log(num1, num2)}
+
+        if(value !==undefined && !isNaN(value)){
+            this.history.push ( (num1) + (action) + (num2) + ' = ' + (value))
+            console.log(( (num1) + (action) + (num2) + ' = ' + (value)))
+            return value
+        }
+    }
 
 const calc = new Calculator();
 let action, promptContent, isCorrectAction, number1, number2;
@@ -85,18 +139,30 @@ do {
     if(isCorrectAction) {
         number1 = prompt('Podaj liczbę nr 1');
         number2 = prompt('Podaj liczbę nr 2');
- 
-        if(action === '+') {
-            calc.sum(number1, number2);
-        }else if(action === '-') {
-            calc.subtract(number1, number2); 
-        }else if(action === '*') {
-            calc.multiply(number1, number2); 
-        }else if(action === '/') {
-            calc.divide(number1, number2); 
-        }else if(action === '^') {
-            calc.power(number1, number2);
-        }
-    }
+
+    calc.calculate(number1, number2)
+
+}
+
+
+} while(calc.isCorrectAction(action))
+
+//     return  console.log(Calculator.prototype.getOperatorFromUser(action))
     
-} while(calc.isCorrectAction(action));
+// }
+// else if (action === false)
+// {
+//     if(action === true){
+//     alert("Wprowadzono błędy operator")}
+//     else if(result ==! true){
+//     console.log("hay")    
+//     }
+// }
+//     if(calc.isCorrectAction(action) === false){
+//         alert("Wprowadzono błędy operator")
+
+//         // return start()
+//     // } else if(calc.isCorrectAction(action) === null){
+    
+//     // }
+//  }
