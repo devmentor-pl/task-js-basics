a2d9ca7669e464a621ed69028c81225d4443e5f4
Ryuuken <wachnickiandrzej@gmail.com>
2023-04-07 18:49:50 +0200

    Adding to history funcionality done

--- js/app.js
+++ js/app.js
@@ -12,11 +12,32 @@ Calculator.prototype.getHistoryAsString = function() {
 }
 
 Calculator.prototype.add = function(num1, num2) {
+    const numb1 = parseInt(num1); 
+    const numb2 = parseInt(num2);
+    if(checkingVariableType([numb1, numb2])) {
+        const result = numb1 + numb2;
+        this.addToHistory(`${numb1} + ${numb2} = ${result}`);
+    } 
     // 1. zamień wartości przekazane przez parametr na typ number
     // 2. sprawdź czy są one poprawne
     // 3. jeśli tak to wykonaj działanie i zapisz jego resultat
     // 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
 }
+
+Calculator.prototype.addToHistory = function(calc) {
+    this.history.push(calc);
+}
+
+function checkingVariableType(arr) {
+    arr.forEach((num) => {
+        if(typeof num !== "number") {
+            return false;
+        }
+    })
+    return true;
+}
+
+
 
 const calc = new Calculator();
 let action, promptContent, isCorrectAction, number1, number2;
@@ -33,6 +54,7 @@ do {
 
         if(action === '+') {
             calc.add(number1, number2);
+            console.log(calc.getHistoryAsString());
         }
     }
     
