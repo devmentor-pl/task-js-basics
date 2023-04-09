e82e3b2dc1148f89e4a5f95956e6c50f6d28c694
Ryuuken <wachnickiandrzej@gmail.com>
2023-04-09 14:38:03 +0200

    Adding add and subtraction funcionalities

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 function Calculator() {
     this.actions = ['+', '-', '*', '/', '^'];
     this.history = [];
+    this.calculationResult = 0;
 }
 
 Calculator.prototype.isCorrectAction = function(action) {
@@ -11,32 +12,64 @@ Calculator.prototype.getHistoryAsString = function() {
     return this.history.join('\n');
 }
 
+Calculator.prototype.getCalculationResult = function() {
+    return this.calculationResult;
+}
+
 Calculator.prototype.add = function(num1, num2) {
-    const numb1 = parseInt(num1); 
-    const numb2 = parseInt(num2);
-    if(checkingVariableType([numb1, numb2])) {
-        const result = numb1 + numb2;
-        this.addToHistory(`${numb1} + ${numb2} = ${result}`);
-    } 
+    this.calculationResult = num1 + num2;
+    // this.addToHistory(`${parsedNumbers[0]} + ${parsedNumbers[1]} = ${result}`);
+    return `${num1} + ${num2} = ${this.calculationResult}`;
     // 1. zamień wartości przekazane przez parametr na typ number
     // 2. sprawdź czy są one poprawne
     // 3. jeśli tak to wykonaj działanie i zapisz jego resultat
     // 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
-}
+};
+
+Calculator.prototype.subtraction = function(number1, number2) {
+    this.calculationResult = number1 - number2;
+    return `${number1} - ${number2} = ${this.calculationResult}`;
+};
+
+Calculator.prototype.multiplication = function(number1, number2) {
+    this.calculationResult = number1 * number2;
+    return `${number1} * ${number2} = ${this.calculationResult}`;
+};
+
+Calculator.prototype.division = function(number1, number2) {
+    this.calculationResult = number1 / number2;
+    return `${number1} / ${number2} = ${this.calculationResult}`;
+};
+
+Calculator.prototype.exponentiation = function(number1, power) {
+    this.calculationResult = number1;
+    for(let i = 1; i <= power; i++) {
+        this.calculationResult *= number1;
+    }
+    return `${number1} ^ ${power} = ${this.calculationResult}`;
+};
 
 Calculator.prototype.addToHistory = function(calc) {
     this.history.push(calc);
 }
 
 function checkingVariableType(arr) {
-    arr.forEach((num) => {
+    arr.forEach(function(num) {
         if(typeof num !== "number") {
-            return false;
+            return 0;
+        } else {
+            return 1;
         }
-    })
-    return true;
-}
+    });
+    
+};
 
+function parsingToInt(number) {
+    if(isNaN(number)) {
+        return 0;
+    }
+    return parseInt(number);
+}
 
 
 const calc = new Calculator();
@@ -49,13 +82,36 @@ do {
     action = prompt(promptContent);
     isCorrectAction = calc.isCorrectAction(action);
     if(isCorrectAction) {
-        number1 = prompt('Podaj liczbę nr 1');
-        number2 = prompt('Podaj liczbę nr 2');
-
-        if(action === '+') {
-            calc.add(number1, number2);
-            console.log(calc.getHistoryAsString());
+        number1 = parsingToInt(prompt('Podaj liczbę nr 1'));
+        number2 = parsingToInt(prompt('Podaj liczbę nr 2'));
+        console.log(typeof number1, typeof number2); 
+        let calcResult = 0;
+        console.log(checkingVariableType([number1, number2]));
+        if(number1 && number2) {
+            switch(action) {
+                case "+":
+                    calcResult = calc.add(number1, number2);
+                    // console.log(calc.getHistoryAsString());
+                    break;
+                case "-":
+                    calcResult = calc.subtraction(number1, number2);
+                    break;
+            }
+            calc.addToHistory(calcResult);
+            console.log(calc.getCalculationResult());
         }
+
+        // if(action === '+') {
+            // calc.add(number1, number2);
+            // console.log(calc.getHistoryAsString());
+        // } else if(action === "-") {
+
+        // } else if(action === "*") {
+
+        // } else if(action === "/") {
+
+        // }
+        
     }
     
 } while(calc.isCorrectAction(action));
