e1b0533ab2246b6a2a35ffddcb01d73b8f395a62
mike161pl <mike161pl@gmail.com>
2023-05-11 21:10:41 +0200

    calculator finished

--- js/app.js
+++ js/app.js
@@ -1,39 +1,72 @@
 function Calculator() {
-    this.actions = ['+', '-', '*', '/', '^'];
-    this.history = [];
+	this.actions = ['+', '-', '*', '/', '^']
+	this.history = []
 }
 
-Calculator.prototype.isCorrectAction = function(action) {
-    return this.actions.includes(action);
+Calculator.prototype.isCorrectAction = function (action) {
+	return this.actions.includes(action)
 }
 
-Calculator.prototype.getHistoryAsString = function() {
-    return this.history.join('\n');
+Calculator.prototype.getHistoryAsString = function () {
+	return this.history.join('\n')
+}
+Calculator.prototype.add = function (num1, num2) {
+	let sum = num1 + num2
+	let sumResult = `${num1} + ${num2} = ${sum}`
+	this.history.push(sumResult)
+}
+Calculator.prototype.subtract = function (num1, num2) {
+	let sub = num1 - num2
+	let subResult = `${num1} - ${num2} = ${sub}`
+	this.history.push(subResult)
+}
+Calculator.prototype.multiply = function (num1, num2) {
+	let multiply = num1 * num2
+	let multiplyResult = `${num1} * ${num2} = ${multiply}`
+	this.history.push(multiplyResult)
+}
+Calculator.prototype.divide = function (num1, num2) {
+	let divide = num1 / num2
+	let divStringResult = `${num1} / ${num2} = ${divide}`
+	this.history.push(divStringResult)
+}
+Calculator.prototype.powers = function (num1, num2) {
+	let exp = num1 ** num2
+	let expStringResult = `${num1} ^ ${num2} = ${exp}`
+	this.history.push(expStringResult)
 }
 
-Calculator.prototype.add = function(num1, num2) {
-    // 1. zamień wartości przekazane przez parametr na typ number
-    // 2. sprawdź czy są one poprawne
-    // 3. jeśli tak to wykonaj działanie i zapisz jego resultat
-    // 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
-}
+const calc = new Calculator()
+let action, promptContent, isCorrectAction, number1, number2
+do {
+	promptContent = 'Podaj jaką operację chcesz wykonać (+, -, *, /, ^) i potwierdź. \n' // \n - znak nowej linii
+	promptContent += 'Jeśli chcesz zrezygnować wciśnij Anuluj. \n'
+	promptContent += 'Lista poprzednich operacji: \n' + calc.getHistoryAsString()
 
-const calc = new Calculator();
-let action, promptContent, isCorrectAction, number1, number2;
-do { 
-    promptContent = 'Podaj jaką operację chcesz wykonać (+, -, *, /, ^) i potwierdź. \n'; // \n - znak nowej linii
-    promptContent += 'Jeśli chcesz zrezygnować wciśnij Anuluj. \n';
-    promptContent += 'Lista poprzednich operacji: \n' + calc.getHistoryAsString();
-
-    action = prompt(promptContent);
-    isCorrectAction = calc.isCorrectAction(action);
-    if(isCorrectAction) {
-        number1 = prompt('Podaj liczbę nr 1');
-        number2 = prompt('Podaj liczbę nr 2');
-
-        if(action === '+') {
-            calc.add(number1, number2);
-        }
-    }
-    
-} while(calc.isCorrectAction(action));
+	action = prompt(promptContent)
+	isCorrectAction = calc.isCorrectAction(action)
+	if (!isCorrectAction) {
+		alert('Błędna wartość. wybierz jedną z nich=(+, -, *, /, ^)')
+	}
+	if (isCorrectAction) {
+		number1 = parseInt(prompt('Podaj liczbę nr 1'))
+		if (isNaN(number1)) {
+			alert('To nie jest liczba-otrzymasz niewłaściwy wynik')
+		}
+		number2 = parseInt(prompt('Podaj liczbę nr 2'))
+		if (isNaN(number2)) {
+			alert('To nie jest liczba-otrzymasz niewłaściwy wynik')
+		}
+		if (action === '+') {
+			calc.add(number1, number2)
+		} else if (action === '-') {
+			calc.subtract(number1, number2)
+		} else if (action === '*') {
+			calc.multiply(number1, number2)
+		} else if (action === '/') {
+			calc.divide(number1, number2)
+		} else if (action === '^') {
+			calc.powers(number1, number2)
+		}
+	}
+} while (calc.isCorrectAction(action))
