530ab7e143b169913ec612eafa76729986c1a4c2
Fryderyk Jellinek <fryderyk.jellinek@gmail.com>
2023-07-25 22:58:33 +0200

    add function instead of switch for every single action

--- js/app.js
+++ js/app.js
@@ -1,67 +1,73 @@
 function Calculator() {
-  this.actions = ["+", "-", "*", "/", "^"];
+  // this.actions = ["+", "-", "*", "/", "^"];
+  this.actions = {
+    '+': this.add,
+    '-': this.subtract,
+    '*': this.multiply,
+    '/': this.divide,
+    '^': this.exponentiation,
+  }
   this.history = [];
 }
 
 Calculator.prototype.isCorrectAction = function (action) {
-  return this.actions.includes(action);
+    // return this.actions.includes(action)
+    return this.actions[action]
 };
 
 Calculator.prototype.getHistoryAsString = function () {
   return this.history.join("\n");
 };
-
-Calculator.prototype.calculate = function (num1, num2) {
-  switch (action) {
-    case "+": {
-      const result = num1 + num2;
-      const resultAdd = `${num1} + ${num2} = ${result}`;
-      this.history.push(resultAdd);
-      return resultAdd;
-    }
-    case "-": {
-      const result = num1 - num2;
-      const resultSubtraction = `${num1} - ${num2} = ${result}`;
-      this.history.push(resultSubtraction);
-      return resultSubtraction;
-    }
-    case "*": {
-      const result = num1 * num2;
-      const resultMultiplication = `${num1} * ${num2} = ${result}`;
-      this.history.push(resultMultiplication);
-      return resultMultiplication;
-    }
-    case "/": {
-      if (num2 === 0) {
-        alert("NIE DZIELIMY PRZEZ 0!!!");
-      } else {
-        const result = num1 / num2;
-        const resultDivision = `${num1} / ${num2} = ${result}`;
-        this.history.push(resultDivision);
-        return resultDivision;
-      }
-    }
-    case "^": {
-      let result = num1 ** num2;
-      let string = "";
-      if (num2 === 0) {
-        result = 1;
-      } else {
-        for (let i = 0; i < num2; i++) {
-          if (i === 0) {
-            string += `${num1}`;
-          } else {
-            string += ` * ${num1}`;
-          }
-        }
-        const resultExponent = `${string} = ${result}`;
-        this.history.push(resultExponent);
-        return resultExponent;
-      }
-    }
+Calculator.prototype.add = function (num1, num2) {
+  const result = num1 + num2;
+  const resultAdd = `${num1} + ${num2} = ${result}`;
+  this.history.push(resultAdd);
+  return resultAdd;
+};
+Calculator.prototype.subtract = function (num1, num2) {
+  const result = num1 - num2;
+  const resultSubtraction = `${num1} - ${num2} = ${result}`;
+  this.history.push(resultSubtraction);
+  return resultSubtraction;
+};
+Calculator.prototype.multiply = function (num1, num2) {
+  const result = num1 * num2;
+  const resultMultiplication = `${num1} * ${num2} = ${result}`;
+  this.history.push(resultMultiplication);
+  return resultMultiplication;
+};
+Calculator.prototype.divide = function (num1, num2) {
+  if (num2 === 0) {
+    alert("NIE DZIELIMY PRZEZ 0!!!");
+  } else {
+    const result = num1 / num2;
+    const resultDivision = `${num1} / ${num2} = ${result}`;
+    this.history.push(resultDivision);
+    return resultDivision;
   }
 };
+Calculator.prototype.exponentiation = function (num1, num2) {
+  let result = num1 ** num2;
+  let string = "";
+  if (num2 === 0) {
+    result = 1;
+  } else {
+    for (let i = 0; i < num2; i++) {
+      if (i === 0) {
+        string += `${num1}`;
+      } else {
+        string += ` * ${num1}`;
+      }
+    }
+    const resultExponent = `${string} = ${result}`;
+    this.history.push(resultExponent);
+    return resultExponent;
+  }
+};
+
+
 const calc = new Calculator();
+
 let action, promptContent, isCorrectAction, number1, number2;
 do {
   promptContent =
@@ -70,13 +76,30 @@ do {
   promptContent += "Lista poprzednich operacji: \n" + calc.getHistoryAsString();
 
   action = prompt(promptContent);
+
+
   isCorrectAction = calc.isCorrectAction(action);
+
   if (isCorrectAction) {
     number1 = parseInt(prompt("Podaj liczbę nr 1"));
     number2 = parseInt(prompt("Podaj liczbę nr 2"));
-
     if (!isNaN(number1) && !isNaN(number2)) {
-      calc.calculate(number1, number2);
+
+      if (action === '+') {
+         calc.add(number1, number2);
+      }
+      if (action === '-') {
+        calc.subtract(number1, number2);
+      }
+      if (action === '*') {
+        calc.multiply(number1, number2);
+      }
+      if (action === '/') {
+        calc.divide(number1, number2)
+      }
+      if (action === '^') {
+        calc.exponentiation(number1, number2)
+      }
     } else {
       console.log("Nie podałeś liczby, Spróbuj jeszcze raz!");
     }
