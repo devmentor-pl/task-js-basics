d5a10b98937284f8d75f6f787cb946b72a191702
Marcin <xcinek1337@wp.pl>
2023-07-31 10:57:22 +0200

    refactoring if

--- js/app.js
+++ js/app.js
@@ -18,10 +18,9 @@ Calculator.prototype.add = function (num1, num2) {
 	// 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
 	const number1 = parseInt(num1);
 	const number2 = parseInt(num2);
-	if (typeof number1 === 'number' && typeof number2 === 'number') {
-		const result = number1 + number2;
-		this.history.push(`${number1} + ${number2} = ${result}`);
-	}
+
+	const result = number1 + number2;
+	this.history.push(`${number1} + ${number2} = ${result}`);
 };
 Calculator.prototype.substraction = function (num1, num2) {
 	const result = num1 - num2;
@@ -33,7 +32,11 @@ Calculator.prototype.multiply = function (num1, num2) {
 };
 Calculator.prototype.divide = function (num1, num2) {
 	const result = num1 / num2;
-	this.history.push(`${num1} / ${num2} = ${result}`);
+	if (num1 == 0 || num2 == 0) {
+		this.history.push('wprowadz poprawne wartosci');
+	} else {
+		this.history.push(`${num1} / ${num2} = ${result}`);
+	}
 };
 Calculator.prototype.increase = function (num1, num2) {
 	let result = 1;
@@ -57,16 +60,15 @@ do {
 		number1 = prompt('Podaj liczbę nr 1');
 		number2 = prompt('Podaj liczbę nr 2');
 
-		if (action === '+') {
-			calc.add(number1, number2);
-		} else if (action === '-') {
-			calc.substraction(number1, number2);
-		} else if (action === '*') {
-			calc.multiply(number1, number2);
-		} else if (action === '/') {
-			calc.divide(number1, number2);
-		} else if (action === '^') {
-			calc.increase(number1, number2);
+		const actions = {
+			'+': calc.add,
+			'-': calc.substraction,
+			'/': calc.divide,
+			'*': calc.multiply,
+			'^': calc.increase,
+		};
+		if (actions[action]) {
+			actions[action].call(calc, number1, number2);
 		}
 	}
 } while (calc.isCorrectAction(action));
