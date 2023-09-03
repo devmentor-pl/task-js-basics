28bbe371ca29b76a64757a97ce57268e248c44b6
Piotr <you@example.com>
2023-09-03 13:41:15 +0200

    submission of the corrected task

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ Calculator.prototype.getHistoryAsString = function () {
 	return this.history.join('\n')
 }
 
-Calculator.prototype.add = function (num1, num2) {
+Calculator.prototype.add = function (num1, num2, metod) {
 	// 1. zamień wartości przekazane przez parametr na typ number
 	num1 = Number(num1)
 	num2 = Number(num2)
@@ -20,72 +20,22 @@ Calculator.prototype.add = function (num1, num2) {
 		return 'wprowadź poprawne dane'
 	}
 	// 3. jeśli tak to wykonaj działanie i zapisz jego resultat
-	const result = num1 + num2
+	const metods = {
+		'+': (a, b) => a + b,
+		'-': (a, b) => a - b,
+		'*': (a, b) => a * b,
+		'/': (a, b) => a / b,
+		'^': (a, b) => a ** b,
+	}
+
+	const result = metods[metod](num1, num2)
 
 	// 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
-	const operationHistory = `${num1} + ${num2} = ${result}`
+	const operationHistory = `${num1} ${metod} ${num2} = ${result}`
 	this.history.push(operationHistory)
 }
-Calculator.prototype.subtract = function (num1, num2) {
-	// 1. zamień wartości przekazane przez parametr na typ number
-	num1 = Number(num1)
-	num2 = Number(num2)
-	// 2. sprawdź czy są one poprawne
-	if (isNaN(num1) || isNaN(num2)) {
-		return 'wprowadź poprawne dane'
-	}
-	// 3. jeśli tak to wykonaj działanie i zapisz jego resultat
-	const result = num1 - num2
 
-	// 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
-	const operationHistory = `${num1}-${num2} = ${result}`
-	this.history.push(operationHistory)
-}
-Calculator.prototype.multiply = function (num1, num2) {
-	// 1. zamień wartości przekazane przez parametr na typ number
-	num1 = Number(num1)
-	num2 = Number(num2)
-	// 2. sprawdź czy są one poprawne
-	if (isNaN(num1) || isNaN(num2)) {
-		return 'wprowadź poprawne dane'
-	}
-	// 3. jeśli tak to wykonaj działanie i zapisz jego resultat
-	const result = num1 * num2
-
-	// 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
-	const operationHistory = `${num1} * ${num2} = ${result}`
-	this.history.push(operationHistory)
-}
-Calculator.prototype.divide = function (num1, num2) {
-	// 1. zamień wartości przekazane przez parametr na typ number
-	num1 = Number(num1)
-	num2 = Number(num2)
-	// 2. sprawdź czy są one poprawne
-	if (isNaN(num1) || isNaN(num2)) {
-		return 'wprowadź poprawne dane'
-	}
-	// 3. jeśli tak to wykonaj działanie i zapisz jego resultat
-	const result = num1 / num2
-
-	// 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
-	const operationHistory = `${num1} / ${num2} = ${result}`
-	this.history.push(operationHistory)
-}
-Calculator.prototype.power = function (num1, num2) {
-	// 1. zamień wartości przekazane przez parametr na typ number
-	num1 = Number(num1)
-	num2 = Number(num2)
-	// 2. sprawdź czy są one poprawne
-	if (isNaN(num1) || isNaN(num2)) {
-		return 'wprowadź poprawne dane'
-	}
-	// 3. jeśli tak to wykonaj działanie i zapisz jego resultat
-	const result = num1 ** num2
-
-	// 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
-	const operationHistory = `${num1} ^ ${num2} = ${result}`
-	this.history.push(operationHistory)
-} 
+const calc = new Calculator()
 let action, promptContent, isCorrectAction, number1, number2
 do {
 	promptContent = 'Podaj jaką operację chcesz wykonać (+, -, *, /, ^) i potwierdź. \n' // \n - znak nowej linii
@@ -97,21 +47,6 @@ do {
 	if (isCorrectAction) {
 		number1 = prompt('Podaj liczbę nr 1')
 		number2 = prompt('Podaj liczbę nr 2')
-
-		if (action === '+') {
-			calc.add(number1, number2)
-		}
-		if (action === '-') {
-			calc.subtract(number1, number2)
-		}
-		if (action === '*') {
-			calc.multiply(number1, number2)
-		}
-		if (action === '/') {
-			calc.divide(number1, number2)
-		}
-		if (action === '^') {
-			calc.power(number1, number2)
-		}
+		calc.add(number1, number2, action)
 	}
 } while (calc.isCorrectAction(action))
