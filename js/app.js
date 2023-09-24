f27e07e2f6184c66a119e8539b2a199e1a500044
Maciej Necka <macieknecka@gmail.com>
2023-09-24 23:11:43 +0200

    NotZero removed, Number conversion fixed, merged calcOptions => actions

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 function Calculator() {
-  this.actions = ['+', '-', '*', '/', '^'];
+  this.actions = {
+    '+': 'add',
+    '-': 'minus',
+    '*': 'multiplication',
+    '/': 'division',
+    '^': 'exponentiation',
+  };
   this.history = [];
 }
 
@@ -11,42 +17,31 @@ Calculator.prototype.getHistoryAsString = function () {
   return this.history.join('\n');
 };
 
-Calculator.prototype.add = function (num1, num2) {
-  const number1 = Number(num1);
-  const number2 = Number(num2);
+Calculator.prototype.add = function (number1, number2) {
   const addResult = number1 + number2;
   this.history.push(`${number1} + ${number2} = ${addResult}`);
 };
 
-Calculator.prototype.minus = function (num1, num2) {
-  const minusResult = num1 - num2;
-  this.history.push(`${num1} - ${num2} = ${minusResult}`);
+Calculator.prototype.minus = function (number1, number2) {
+  const minusResult = number1 - number2;
+  this.history.push(`${number1} - ${number2} = ${minusResult}`);
 };
 
-Calculator.prototype.multiplication = function (num1, num2) {
-  const multiResult = num1 * num2;
-  this.history.push(`${num1} * ${num2} = ${multiResult}`);
+Calculator.prototype.multiplication = function (number1, number2) {
+  const multiResult = number1 * number2;
+  this.history.push(`${number1} * ${number2} = ${multiResult}`);
 };
 
-Calculator.prototype.division = function (num1, num2) {
-  const number2 = notZero(Number(num2));
-  const divisionResult = (num1 / number2).toFixed(2);
-  this.history.push(`${num1} / ${number2} = ${divisionResult}`);
-};
-
-function notZero(number2) {
-  number2 = +number2;
-  if (!number2) {
-    throw console.error(
-      prompt('Nie można dzielić przez 0! Wcisnij F5 aby kontynuować')
-    );
+Calculator.prototype.division = function (number1, number2) {
+  if (Number(number2) !== 0) {
+    const divisionResult = (number1 / number2).toFixed(2);
+    this.history.push(`${number1} / ${number2} = ${divisionResult}`);
+  } else {
+    throw new Error('Nie możesz wykonać dzielenia przez 0!');
   }
-  return number2;
-}
+};
 
-Calculator.prototype.exponentiation = function (num1, num2) {
-  const number1 = Number(num1);
-  const number2 = Number(num2);
+Calculator.prototype.exponentiation = function (number1, number2) {
   let expResult = 1;
   for (let i = 0; i < number2; i++) {
     expResult *= number1;
@@ -63,20 +58,11 @@ do {
   promptContent += 'Lista poprzednich operacji: \n' + calc.getHistoryAsString();
 
   action = prompt(promptContent);
-  isCorrectAction = calc.isCorrectAction(action);
+  isCorrectAction = Object.hasOwnProperty.call(calc.actions, action);
   if (isCorrectAction) {
-    number1 = prompt('Podaj liczbę nr 1');
-    number2 = prompt('Podaj liczbę nr 2');
+    number1 = Number(prompt('Podaj liczbę nr 1'));
+    number2 = Number(prompt('Podaj liczbę nr 2'));
 
-    const calcOptions = {
-      '+': calc.add,
-      '-': calc.minus,
-      '*': calc.multiplication,
-      '/': calc.division,
-      '^': calc.division,
-    };
-    if (calcOptions[action]) {
-      calcOptions[action].call(calc, number1, number2);
-    }
+    calc[calc.actions[action]](number1, number2);
   }
-} while (calc.isCorrectAction(action));
+} while (isCorrectAction);
