4c319ecfcacc694e7f7841e10dbabda9bd4df04b
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2023-09-27 19:04:28 +0200

    confirm changes

--- js/Calculator.js
+++ js/Calculator.js
@@ -35,7 +35,7 @@ export class Calculator {
         for (let i = 0; i < amount; i++) {
             const ulElem = document.createElement("ul");
             ulElem.classList.add("operations__list");
-           this.operationsSection.appendChild(ulElem); 
+            this.operationsSection.appendChild(ulElem);
             this.createLi(2, ulElem)
         }
     }
@@ -65,298 +65,298 @@ export class Calculator {
         this.putTextToButton('.operations__button', array)
     }
 
-createContentOfOperations(array) {
-    this.createUl(this.calculateActions.length)
-    this.createButton('.operations__list',array)
-}
-
-isCorrectName(greeting) {
-    const name = document.querySelector('.main__heading');
-    name.innerText = `Hi ${greeting}!`
-    return name
-}
-
-isCorrectAction(action) {
-    return this.actions.includes(action);
-}
-
-getHistoryAsString() {
-    return this.history.join('\n');
-}
-
-isCorrectNumber(val1, val2) {
-
-
-    if (!(isNaN(val1)) && !(isNaN(val2))) {
-        return true
-    } else {
-        return alert('podaj liczbę')
-    }
-}
-
-addToHistory(num1, num2, action, result) {
-    return this.history.push(num1 + ' ' + action + ' ' + num2 + ' ' + '=' + ' ' + result);
-}
-
-add(num1, num2, action, type) {
-
-    let result;
-    result = parseInt(num1) + parseInt(num2)
-
-    const addParent = document.querySelector(`#${type}`).parentElement
-    const li = addParent.querySelectorAll('li')
-    const arrayli = [...li]
-    this.operationAddHistory.push(result)
-    const pairedArray = [];
-
-    for (let i = 0; i < this.operationAddHistory.length; i++) {
-        pairedArray.push({ item: this.operationAddHistory[i], value: arrayli[i] })
-        pairedArray.forEach(function (el) {
-            if (el && el.value !== undefined) {
-                el.value.innerText = el.item
-            } else {
-                alert(`choose another operation without ${action}`)
-                const showResult = document.querySelector(`.${type}`)
-                showResult.remove()
-            }
-        })
+    createContentOfOperations(array) {
+        this.createUl(this.calculateActions.length)
+        this.createButton('.operations__list', array)
     }
 
-    let operationResult
-    operationResult = parseFloat(num1) + action + parseFloat(num2) + ' = ' + result
-    this.history.push(operationResult)
-    const history = document.querySelector(".main__history")
-    history.innerText = `Lista poprzednich operacji: \n` + this.getHistoryAsString()
-    return operationResult
-
-}
-
-subtract(num1, num2, action, type) {
-
-    let result;
-    result = parseInt(num1) - parseInt(num2)
-
-
-    const addParent = document.querySelector(`#${type}`).parentElement
-    const li = addParent.querySelectorAll('li')
-    const arrayli = [...li]
-    this.operationSubtractHistory.push(result)
-
-    const pairedArray = [];
-
-    for (let i = 0; i < this.operationSubtractHistory.length; i++) {
-        pairedArray.push({ item: this.operationSubtractHistory[i], value: arrayli[i] })
-        pairedArray.forEach(function (el) {
-
-            if (el && el.value !== undefined) {
-                el.value.innerText = el.item
-            } else {
-                alert(`choose another operation without ${action}`)
-                const showResult = document.querySelector(`.${type}`)
-                showResult.remove()
-            }
-        })
+    isCorrectName(greeting) {
+        const name = document.querySelector('.main__heading');
+        name.innerText = `Hi ${greeting}!`
+        return name
     }
 
-    let operationResult
-    operationResult = parseFloat(num1) + action + parseFloat(num2) + ' = ' + result
-
-    this.history.push(operationResult)
-    const history = document.querySelector(".main__history")
-    history.innerText = `Lista poprzednich operacji: \n'` + this.getHistoryAsString()
-    return operationResult
-
-}
-
-
-multiply(num1, num2, action, type) {
-
-    let result;
-    result = parseInt(num1) + parseInt(num2)
-
-    const addParent = document.querySelector(`#${type}`).parentElement
-    const li = addParent.querySelectorAll('li')
-    const arrayli = [...li]
-    this.operationMultiplyHistory.push(result)
-
-    const pairedArray = [];
-
-    for (let i = 0; i < this.operationMultiplyHistory.length; i++) {
-        pairedArray.push({ item: this.operationMultiplyHistory[i], value: arrayli[i] })
-        pairedArray.forEach(function (el) {
-
-            if (el && el.value !== undefined) {
-                el.value.innerText = el.item
-            } else {
-                alert(`choose another operation without ${action}`)
-                const showResult = document.querySelector(`.${type}`)
-                showResult.remove()
-            }
-        })
+    isCorrectAction(action) {
+        return this.actions.includes(action);
     }
 
-    let operationResult
-    operationResult = parseFloat(num1) + action + parseFloat(num2) + ' = ' + result
-
-    this.history.push(operationResult)
-    const history = document.querySelector(".main__history")
-    history.innerText = `Lista poprzednich operacji: \n` + this.getHistoryAsString()
-    return operationResult
-
-}
-
-
-divide(num1, num2, action, type) {
-
-    let result;
-    result = parseInt(num1) / parseInt(num2)
-
-
-    const addParent = document.querySelector(`#${type}`).parentElement
-    const li = addParent.querySelectorAll('li')
-    const arrayli = [...li]
-    this.operationDivideHistory.push(result)
-
-    const pairedArray = [];
-
-    for (let i = 0; i < this.operationDivideHistory.length; i++) {
-        pairedArray.push({ item: this.operationDivideHistory[i], value: arrayli[i] })
-        pairedArray.forEach(function (el) {
-
-            if (el && el.value !== undefined) {
-                el.value.innerText = el.item
-            } else {
-                alert(`choose another operation without ${action}`)
-                const showResult = document.querySelector(`.${type}`)
-                showResult.remove()
-            }
-        })
+    getHistoryAsString() {
+        return this.history.join('\n');
     }
 
-    let operationResult
-    operationResult = parseFloat(num1) + action + parseFloat(num2) + ' = ' + result
-
-    this.history.push(operationResult)
-    const history = document.querySelector(".main__history")
-    history.innerText = `Lista poprzednich operacji: \n` + this.getHistoryAsString()
-    return operationResult
-
-}
-
-power(num1, num2, powerNumber, action, type) {
+    isCorrectNumber(val1, val2) {
 
 
-    let result2;
-
-    this.num1 = num1
-    this.powerNumber = powerNumber
-    let result
-    let i;
-
-    if (powerNumber == 0) {
-        return 1;
-    } else {
-        let temp = num1;
-        for (i = 0; i < powerNumber - 1; i++) {
-            num1 = num1 * temp;
-
-            result = num1
-
+        if (!(isNaN(val1)) && !(isNaN(val2))) {
+            return true
+        } else {
+            return alert('podaj liczbę')
         }
     }
 
-    const addParent = document.querySelector(`#${type}`).parentElement
-    const li = addParent.querySelectorAll('li')
-    const arrayli = [...li]
-    this.operationPowerHistory.push(result)
-
-    const pairedArray = [];
-
-    for (let i = 0; i < this.operationPowerHistory.length; i++) {
-        pairedArray.push({ item: this.operationPowerHistory[i], value: arrayli[i] })
-        pairedArray.forEach(function (el) {
-
-            if (el && el.value !== undefined) {
-                el.value.innerText = el.item
-            } else {
-                alert(`choose another operation without ${action}`)
-                const showResult = document.querySelector(`.${type}`)
-                showResult.remove()
-            }
-        })
+    addToHistory(num1, num2, action, result) {
+        return this.history.push(num1 + ' ' + action + ' ' + num2 + ' ' + '=' + ' ' + result);
     }
 
-    let operationResult
-    operationResult = parseFloat(num1) + action + parseFloat(num2) + ' = ' + result2
+    add(num1, num2, action, type) {
 
-    this.history.push(operationResult)
-    const history = document.querySelector(".main__history")
-    history.innerText = `Lista poprzednich operacji: \n` + this.getHistoryAsString()
-    return operationResult
+        let result;
+        result = parseInt(num1) + parseInt(num2)
 
+        const addParent = document.querySelector(`#${type}`).parentElement
+        const li = addParent.querySelectorAll('li')
+        const arrayli = [...li]
+        this.operationAddHistory.push(result)
+        const pairedArray = [];
 
-}
-
-start(calc) {
-
-    let action, isNumber, result, greeting, greetingContent
-
-    greetingContent = 'Please write your name'
-    greeting = prompt(greetingContent)
-    if (greeting) {
-        calc.isCorrectName(greeting)
-    }
-
-    this.resultsSection.setAttribute('style', 'display:block')
-    this.operationsSection.setAttribute('style', 'display:block')
-
-    do {
-
-
-        const operations = ['+', '-', '*', '/', '**']
-        const buttonSubmit = document.querySelector('.main__buttonSubmit')
-        buttonSubmit.addEventListener('click', (event) => {
-            event.preventDefault()
-            const action = document.querySelector('.operations--type').value
-
-            let val1 = document.querySelector('.number1').value
-            let val2 = document.querySelector('.number2').value
-            let powerNumber = document.querySelector('.powerNumber').value
-
-            isNumber = this.isCorrectNumber(val1, val2)
-
-            if (operations.includes((action))) {
-                let operator, type;
-                switch (action) {
-                    case '+': if (isNumber) {
-                        result = calc.add(val1, val2, operator = '+', type = 'add')
-                    };
-                        break;
-                    case '-': if (isNumber) {
-                        result = calc.subtract(val1, val2, operator = '-', type = 'subtract')
-                    };
-                        break;
-                    case '*': if (isNumber) {
-                        result = calc.multiply(val1, val2, operator = '*', type = 'multiply')
-                    };
-                        break;
-                    case '/': if (isNumber) {
-                        result = calc.divide(val1, val2, operator = '/', type = 'divide')
-                    };
-                        break
-                    case '**': if (isNumber) {
-
-                        result = calc.power(val1, val2, powerNumber, operator = '**', type = 'power')
-                    };
-
+        for (let i = 0; i < this.operationAddHistory.length; i++) {
+            pairedArray.push({ item: this.operationAddHistory[i], value: arrayli[i] })
+            pairedArray.forEach(function (el) {
+                if (el && el.value !== undefined) {
+                    el.value.innerText = el.item
+                } else {
+                    alert(`choose another operation without ${action}`)
+                    const showResult = document.querySelector(`.${type}`)
+                    showResult.remove()
                 }
-            } else {
-                alert(`${action} is not in the array.`);
-            }
-        })
+            })
+        }
 
-    } while (calc.isCorrectAction(action));
-}
+        let operationResult
+        operationResult = parseFloat(num1) + action + parseFloat(num2) + ' = ' + result
+        this.history.push(operationResult)
+        const history = document.querySelector(".main__history")
+        history.innerText = `Lista poprzednich operacji: \n` + this.getHistoryAsString()
+        return operationResult
+
+    }
+
+    subtract(num1, num2, action, type) {
+
+        let result;
+        result = parseInt(num1) - parseInt(num2)
+
+
+        const addParent = document.querySelector(`#${type}`).parentElement
+        const li = addParent.querySelectorAll('li')
+        const arrayli = [...li]
+        this.operationSubtractHistory.push(result)
+
+        const pairedArray = [];
+
+        for (let i = 0; i < this.operationSubtractHistory.length; i++) {
+            pairedArray.push({ item: this.operationSubtractHistory[i], value: arrayli[i] })
+            pairedArray.forEach(function (el) {
+
+                if (el && el.value !== undefined) {
+                    el.value.innerText = el.item
+                } else {
+                    alert(`choose another operation without ${action}`)
+                    const showResult = document.querySelector(`.${type}`)
+                    showResult.remove()
+                }
+            })
+        }
+
+        let operationResult
+        operationResult = parseFloat(num1) + action + parseFloat(num2) + ' = ' + result
+
+        this.history.push(operationResult)
+        const history = document.querySelector(".main__history")
+        history.innerText = `Lista poprzednich operacji: \n'` + this.getHistoryAsString()
+        return operationResult
+
+    }
+
+
+    multiply(num1, num2, action, type) {
+
+        let result;
+        result = parseInt(num1) + parseInt(num2)
+
+        const addParent = document.querySelector(`#${type}`).parentElement
+        const li = addParent.querySelectorAll('li')
+        const arrayli = [...li]
+        this.operationMultiplyHistory.push(result)
+
+        const pairedArray = [];
+
+        for (let i = 0; i < this.operationMultiplyHistory.length; i++) {
+            pairedArray.push({ item: this.operationMultiplyHistory[i], value: arrayli[i] })
+            pairedArray.forEach(function (el) {
+
+                if (el && el.value !== undefined) {
+                    el.value.innerText = el.item
+                } else {
+                    alert(`choose another operation without ${action}`)
+                    const showResult = document.querySelector(`.${type}`)
+                    showResult.remove()
+                }
+            })
+        }
+
+        let operationResult
+        operationResult = parseFloat(num1) + action + parseFloat(num2) + ' = ' + result
+
+        this.history.push(operationResult)
+        const history = document.querySelector(".main__history")
+        history.innerText = `Lista poprzednich operacji: \n` + this.getHistoryAsString()
+        return operationResult
+
+    }
+
+
+    divide(num1, num2, action, type) {
+
+        let result;
+        result = parseInt(num1) / parseInt(num2)
+
+
+        const addParent = document.querySelector(`#${type}`).parentElement
+        const li = addParent.querySelectorAll('li')
+        const arrayli = [...li]
+        this.operationDivideHistory.push(result)
+
+        const pairedArray = [];
+
+        for (let i = 0; i < this.operationDivideHistory.length; i++) {
+            pairedArray.push({ item: this.operationDivideHistory[i], value: arrayli[i] })
+            pairedArray.forEach(function (el) {
+
+                if (el && el.value !== undefined) {
+                    el.value.innerText = el.item
+                } else {
+                    alert(`choose another operation without ${action}`)
+                    const showResult = document.querySelector(`.${type}`)
+                    showResult.remove()
+                }
+            })
+        }
+
+        let operationResult
+        operationResult = parseFloat(num1) + action + parseFloat(num2) + ' = ' + result
+
+        this.history.push(operationResult)
+        const history = document.querySelector(".main__history")
+        history.innerText = `Lista poprzednich operacji: \n` + this.getHistoryAsString()
+        return operationResult
+
+    }
+
+    power(num1, num2, powerNumber, action, type) {
+
+
+        let result2;
+
+        this.num1 = num1
+        this.powerNumber = powerNumber
+        let result
+        let i;
+
+        if (powerNumber == 0) {
+            return 1;
+        } else {
+            let temp = num1;
+            for (i = 0; i < powerNumber - 1; i++) {
+                num1 = num1 * temp;
+
+                result = num1
+
+            }
+        }
+
+        const addParent = document.querySelector(`#${type}`).parentElement
+        const li = addParent.querySelectorAll('li')
+        const arrayli = [...li]
+        this.operationPowerHistory.push(result)
+
+        const pairedArray = [];
+
+        for (let i = 0; i < this.operationPowerHistory.length; i++) {
+            pairedArray.push({ item: this.operationPowerHistory[i], value: arrayli[i] })
+            pairedArray.forEach(function (el) {
+
+                if (el && el.value !== undefined) {
+                    el.value.innerText = el.item
+                } else {
+                    alert(`choose another operation without ${action}`)
+                    const showResult = document.querySelector(`.${type}`)
+                    showResult.remove()
+                }
+            })
+        }
+
+        let operationResult
+        operationResult = parseFloat(num1) + action + parseFloat(num2) + ' = ' + result2
+
+        this.history.push(operationResult)
+        const history = document.querySelector(".main__history")
+        history.innerText = `Lista poprzednich operacji: \n` + this.getHistoryAsString()
+        return operationResult
+
+
+    }
+
+    start(calc) {
+
+        let action, isNumber, result, greeting, greetingContent
+
+        greetingContent = 'Please write your name'
+        greeting = prompt(greetingContent)
+        if (greeting) {
+            calc.isCorrectName(greeting)
+        }
+
+        this.resultsSection.setAttribute('style', 'display:block')
+        this.operationsSection.setAttribute('style', 'display:block')
+
+        do {
+
+
+            const operations = ['+', '-', '*', '/', '**']
+            const buttonSubmit = document.querySelector('.main__buttonSubmit')
+            buttonSubmit.addEventListener('click', (event) => {
+                event.preventDefault()
+                const action = document.querySelector('.operations--type').value
+
+                let val1 = document.querySelector('.number1').value
+                let val2 = document.querySelector('.number2').value
+                let powerNumber = document.querySelector('.powerNumber').value
+
+                isNumber = this.isCorrectNumber(val1, val2)
+
+                if (operations.includes((action))) {
+                    let operator, type;
+                    switch (action) {
+                        case '+': if (isNumber) {
+                            result = calc.add(val1, val2, operator = '+', type = 'add')
+                        };
+                            break;
+                        case '-': if (isNumber) {
+                            result = calc.subtract(val1, val2, operator = '-', type = 'subtract')
+                        };
+                            break;
+                        case '*': if (isNumber) {
+                            result = calc.multiply(val1, val2, operator = '*', type = 'multiply')
+                        };
+                            break;
+                        case '/': if (isNumber) {
+                            result = calc.divide(val1, val2, operator = '/', type = 'divide')
+                        };
+                            break
+                        case '**': if (isNumber) {
+
+                            result = calc.power(val1, val2, powerNumber, operator = '**', type = 'power')
+                        };
+
+                    }
+                } else {
+                    alert(`${action} is not in the array.`);
+                }
+            })
+
+        } while (calc.isCorrectAction(action));
+    }
 }
 
 
