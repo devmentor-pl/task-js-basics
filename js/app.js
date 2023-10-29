5abd44d26b86a06baeb9607d36beff5bd05a7141
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2023-10-29 14:02:30 +0100

    add some changes

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 
 
 import { CalculatorTwo } from "./CalculatorTwo.js";
+import { CalculatorThree } from "./CalculatorThree.js";
 import { forwardPage } from "./forwardPage.js";
 
 const init = () => {
-    const calc2 = new CalculatorTwo();
-    calc2.start(calc2)
+    const calc2 = new CalculatorThree();
+    calc2.render()
+    // calc2.start(calc2)
     forwardPage()
 }
 
