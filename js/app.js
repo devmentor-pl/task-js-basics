47e24e06416a273ded6bb180244275b000adbee7
Aleksy Zydlewski <limeakiwano33@gmail.com>
2023-10-29 20:25:43 +0100

    tez nie działa

--- js/app.js
+++ js/app.js
@@ -15,19 +15,75 @@ Calculator.prototype.add = function(number1, number2) {
     number1 = Number(number1)
     number2 = Number(number2)
     const result = number1 + number2
-    if(typeof number1 == Number || typeof number2 == Number) {
+    if(typeof number1 == Number && typeof number2 == Number) {
        return result
     }
     else {
        console.log('podane wartości są niepoprawne') ;
 
     }
+}
+Calculator.prototype.sub = function(number1, number2) {
+    number1 = Number(number1)
+    number2 = Number(number2)
+    
+    if(typeof number1 == Number && typeof number2 == Number) {
+      const result = number1 - number2;
+      
+      return result
+    }
+    else {
+       console.log('podane wartości są niepoprawne') ;
+
+    }
+}
+Calculator.prototype.multi = function(number1, number2) {
+    number1 = Number(number1)
+    number2 = Number(number2)
+    
+    if(typeof number1 == Number && typeof number2 == Number) {
+       const result = number1 * number2;
+        return result
+    }
+    else {
+       console.log('podane wartości są niepoprawne') ;
+
+    }
+}
+Calculator.prototype.div = function(number1, number2) {
+    number1 = Number(number1)
+    number2 = Number(number2)
+    
+    if(typeof number1 == Number && typeof number2 == Number) {
+        const result = number1 / number2
+       return result
+    }
+    else {
+       console.log('podane wartości są niepoprawne') ;
+
+    }
+}
+Calculator.prototype.expo = function(number1, number2) {
+    number1 = Number(number1)
+    number2 = Number(number2)
+    
+    if(typeof number1 == Number && typeof number2 == Number) {
+        const result = number1 ** number2;
+       return result
+    }
+    else {
+       console.log('podane wartości są niepoprawne') ;
+
+    }
+}
+
+
     
     // 1. zamień wartości przekazane przez parametr na typ number
     // 2. sprawdź czy są one poprawne
     // 3. jeśli tak to wykonaj działanie i zapisz jego resultat
     // 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
-}
+
 
 const calc = new Calculator();
 let action, promptContent, isCorrectAction, number1, number2;
@@ -39,12 +95,29 @@ do {
     action = prompt(promptContent);
     isCorrectAction = calc.isCorrectAction(action);
     if(isCorrectAction) {
-        number1 = prompt('Podaj liczbę nr 1');
-        number2 = prompt('Podaj liczbę nr 2');
+        number1 = parseInt(prompt('Podaj liczbę nr 1'));
+        number2 = parseInt(prompt('Podaj liczbę nr 2'));
 
         if(action === '+') {
-            calc.add(number1, number2);
+          result =  calc.add(number1, number2);
+          console.log(result)
         }
+        else if(action === "-") {
+           result = calc.sub(number1, number2);
+           console.log(result)
+        }
+        else if(action === "*") {
+           result = calc.multi(number1, number2);
+            console.log(result)
+        }
+        else if (action === "/") {
+           result = calc.div(number1,number2);
+           console.log(result)
+        }
+        else {
+            calc.expo(number1,number2);
+        }
+
     }
     
 } while(calc.isCorrectAction(action));
