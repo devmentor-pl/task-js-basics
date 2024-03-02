acc5e62e6a12db2d600fc3cb0e8ff4a6df83fd7a
Katarzyna Dworak <k.dw@poczta.onet.pl>
2024-03-02 11:27:04 +0100

    DRY rule completed

--- js/app.js
+++ js/app.js
@@ -3,13 +3,6 @@ function Calculator() {
     this.history = [];
 }
 
-const operations = {
-'+': Calculator.add,
-'-': Calculator.sub,
-'*': Calculator.mul,
-'/': Calculator.div,
-}
-
 Calculator.prototype.isCorrectAction = function(action) {
     return this.actions.includes(action);
 }
@@ -29,31 +22,25 @@ Calculator.prototype.checkIsNan = function(num1, num2) {
 }
 
 Calculator.prototype.transform = function(operationType, num1, num2) {
-    const num1Converted = Number(num1);
-    const num2Converted = Number(num2);
-
-    calc.checkIsNan(num1, num1);
-
-    const result = operations[operationType](num1, num1);
-
-    this.history.push(num1Converted + action + num2Converted + ' = ' + result);
-    console.log(this.history); 
+    calc.checkIsNan(Number(num1), Number(num2));
+    const result = operations[operationType](Number(num1), Number(num2));
+    this.history.push(Number(num1) + action + Number(num2) + ' = ' + result);
 }
 
 Calculator.prototype.add = function(num1, num2) {
-    const result = num1 + num2;
+    return num1 + num2; 
 }
 
 Calculator.prototype.sub = function(num1, num2) {
-    const result = num1 - num2;
+    return num1 + num2; 
     }
 
 Calculator.prototype.mul = function(num1, num2) {
-    const result = num1 * num2;
+    return num1 + num2; 
 }
 
 Calculator.prototype.div = function(num1, num2) {
-    const result = num1 / num2;
+    return num1 + num2; 
 }
 
 Calculator.prototype.pow = function(num1, num2) {
@@ -61,77 +48,19 @@ Calculator.prototype.pow = function(num1, num2) {
     for(let i = 0; i < num2; i ++) {
         result *= num1;
     }
+    return result;
 }
-
-// Calculator.prototype.add = function(num1, num2) {
-
-//     const num1Converted = Number(num1);
-//     const num2Converted = Number(num2);
-
-//     calc.checkIsNan(num1, num1);
-
-//     const result = num1Converted + num2Converted;
-
-//     this.history.push(num1Converted + action + num2Converted + ' = ' + result);
-//     console.log(this.history);
-// }
-
-// Calculator.prototype.sub = function(num1, num2) {
-    
-//     const num1Converted = Number(num1);
-//     const num2Converted = Number(num2);
-    
-//     calc.checkIsNan(num1, num1);
-
-//     const result = num1Converted - num2Converted;
-    
-//     this.history.push(num1Converted + action + num2Converted + ' = ' + result);
-//     console.log(this.history);
-//     }
-
-// Calculator.prototype.mul = function(num1, num2) {
-
-//     const num1Converted = Number(num1);
-//     const num2Converted = Number(num2);
-
-//     calc.checkIsNan(num1, num1);
-
-//     const result = num1Converted * num2Converted;
-
-//     this.history.push(num1Converted + action + num2Converted + ' = ' + result);
-//     console.log(this.history);
-// }
-
-// Calculator.prototype.div = function(num1, num2) {
-
-//     const num1Converted = Number(num1);
-//     const num2Converted = Number(num2);
-
-//     calc.checkIsNan(num1, num1);
-
-//     const result = num1Converted / num2Converted;
-
-//     this.history.push(num1Converted + action + num2Converted + ' = ' + result);
-//     console.log(this.history);
-// }
-
-// Calculator.prototype.pow = function(num1, num2) {
-//     const num1Converted = Number(num1);
-//     const num2Converted = Number(num2);
-
-//     calc.checkIsNan(num1, num1);
-    
-//     let result = 1;
-//     for(let i = 0; i < num2Converted; i ++) {
-//         result *= num1Converted;
-//     }
-
-//     this.history.push(num1Converted + action + num2Converted + ' = ' + result);
-//     console.log(this.history);
-// }
 
 const calc = new Calculator();
 let action, promptContent, isCorrectAction, number1, number2;
+
+const operations = {
+'+': calc.add,
+'-': calc.sub,
+'*': calc.mul,
+'/': calc.div,
+'^': calc.pow,
+}
 
 do { 
     promptContent = 'Podaj jaką operację chcesz wykonać (+, -, *, /, ^) i potwierdź. \n'; // \n - znak nowej linii
@@ -145,53 +74,7 @@ do {
         number1 = prompt('Podaj liczbę nr 1');
         number2 = prompt('Podaj liczbę nr 2');
 
-
-    calc.transform(promptContent, number1, number2);
-        // if(action === '+') {
-        //     calc.add(number1, number2);
-        // } else if(action === '-') {
-        //     calc.sub(number1, number2);
-        // } else if(action === '*') {
-        //     calc.mul(number1, number2);
-        // } else if(action === '/') {
-        //     calc.div(number1, number2);
-        // } else if(action === '^') {
-        //     calc.pow(number1, number2);
-        // } else {
-        //     prompt('Podana została błędna dana');
-        // }
+    calc.transform(action, number1, number2);
     }
     
 } while(calc.isCorrectAction(action));
-
-
-// const calc = new Calculator();
-// let action, promptContent, isCorrectAction, number1, number2;
-// do { 
-//     promptContent = 'Podaj jaką operację chcesz wykonać (+, -, *, /, ^) i potwierdź. \n'; // \n - znak nowej linii
-//     promptContent += 'Jeśli chcesz zrezygnować wciśnij Anuluj. \n';
-//     promptContent += 'Lista poprzednich operacji: \n' + calc.getHistoryAsString();
-
-//     action = prompt(promptContent);
-//     isCorrectAction = calc.isCorrectAction(action);
-
-//     if(isCorrectAction) {
-//         number1 = prompt('Podaj liczbę nr 1');
-//         number2 = prompt('Podaj liczbę nr 2');
-
-//         if(action === '+') {
-//             calc.add(number1, number2);
-//         } else if(action === '-') {
-//             calc.sub(number1, number2);
-//         } else if(action === '*') {
-//             calc.mul(number1, number2);
-//         } else if(action === '/') {
-//             calc.div(number1, number2);
-//         } else if(action === '^') {
-//             calc.pow(number1, number2);
-//         } else {
-//             prompt('Podana została błędna dana');
-//         }
-//     }
-    
-// } while(calc.isCorrectAction(action));
