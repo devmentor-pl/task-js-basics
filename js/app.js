b6367d7e46eee9084196a0b07d8a693dd9cf135f
Paweł Suder <pawel.suder94@gmail.com>
2024-03-13 16:45:20 +0100

    (add,subtract,multiply,divide,powers) functions added

--- js/app.js
+++ js/app.js
@@ -3,37 +3,61 @@ function Calculator() {
     this.history = [];
 }
 
-Calculator.prototype.isCorrectAction = function(action) {
+Calculator.prototype.isCorrectAction = function (action) {
     return this.actions.includes(action);
 }
 
-Calculator.prototype.getHistoryAsString = function() {
+Calculator.prototype.getHistoryAsString = function () {
     return this.history.join('\n');
 }
 
-Calculator.prototype.add = function(num1, num2) {
-    // 1. zamień wartości przekazane przez parametr na typ number
-    // 2. sprawdź czy są one poprawne
-    // 3. jeśli tak to wykonaj działanie i zapisz jego resultat
-    // 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
+Calculator.prototype.add = function (num1, num2) {
+    const sum = num1 + num2
+    const result = num1 + ' + ' + num2 + ' = ' + sum
+    this.history.push(result)
 }
+
+Calculator.prototype.subtract = function (num1, num2) {
+    const subtract = num1 - num2
+    const result = num1 + ' - ' + num2 + ' = ' + subtract
+    this.history.push(result)
+}
+
+Calculator.prototype.multiply = function (num1, num2) {
+    const multiply = num1 * num2
+    const result = num1 + ' * ' + num2 + ' = ' + multiply
+    this.history.push(result)
+}
+
+Calculator.prototype.divide = function (num1, num2) {
+    const divide = num1 / num2
+    const result = num1 + ' / ' + num2 + ' = ' + divide
+    this.history.push(result)
+}
+
+Calculator.prototype.powers = function (num1, num2) {
+    const powers = num1 ** num2
+    const result = num1 + ' ^ ' + num2 + ' = ' + powers
+    this.history.push(result)
+}
+
 
 const calc = new Calculator();
 let action, promptContent, isCorrectAction, number1, number2;
-do { 
+do {
     promptContent = 'Podaj jaką operację chcesz wykonać (+, -, *, /, ^) i potwierdź. \n'; // \n - znak nowej linii
     promptContent += 'Jeśli chcesz zrezygnować wciśnij Anuluj. \n';
     promptContent += 'Lista poprzednich operacji: \n' + calc.getHistoryAsString();
 
     action = prompt(promptContent);
     isCorrectAction = calc.isCorrectAction(action);
-    if(isCorrectAction) {
+    if (isCorrectAction) {
         number1 = prompt('Podaj liczbę nr 1');
         number2 = prompt('Podaj liczbę nr 2');
 
-        if(action === '+') {
+        if (action === '+') {
             calc.add(number1, number2);
         }
     }
-    
-} while(calc.isCorrectAction(action));
+
+} while (calc.isCorrectAction(action));
