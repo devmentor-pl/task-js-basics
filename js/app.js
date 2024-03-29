bfa14b475ccbd4c3fa3e281bc1bd487bb6e9b9bc
Jarek Stelmach <j.stelmach@onet.eu>
2024-03-29 13:27:23 +0100

    Made adding to history part

--- js/app.js
+++ js/app.js
@@ -11,28 +11,23 @@ Calculator.prototype.getHistoryAsString = function () {
 	return this.history.join("\n")
 }
 
+Calculator.prototype.addToHistory = function (value) {
+    return this.history.push(value)
+}
+
 // ADD
 Calculator.prototype.add = function (num1, num2) {
 	const number1 = Number(num1)
 	const number2 = Number(num2)
 
-	// console.log(typeof number1)
-	// console.log(typeof number2)
-
 	const addResult = number1 + number2
-
-	// console.log(addResult);
 	const addOperation = `${number1} + ${number2} = ${addResult}`
 
-	// Calculator.prototype.addResultToHistory = function (result) {
-	//     this.history.push(result)
-	// }
-
-	// Calculator.addResultToHistory(addOperation)
-	// console.log(this.history);
-
+    calc.addToHistory(addOperation)
+    console.log(this.history);
+    
 	return addResult
-
+    
 	// 1. zamień wartości przekazane przez parametr na typ number
 	// 2. sprawdź czy są one poprawne
 	// 3. jeśli tak to wykonaj działanie i zapisz jego resultat
@@ -41,67 +36,79 @@ Calculator.prototype.add = function (num1, num2) {
 
 //SUBSTRACT
 Calculator.prototype.substract = function (num1, num2) {
-	const number1 = Number(num1)
+    const number1 = Number(num1)
 	const number2 = Number(num2)
-
+    
 	const substractResult = number1 - number2
-
+    
 	const substractOperation = `${number1} - ${number2} = ${substractResult}`
-
+    
+    calc.addToHistory(substractOperation)
+    console.log(this.history);
 	console.log(substractResult)
 	return substractResult
 }
 
 //multiply
 Calculator.prototype.multiply = function (num1, num2) {
-	const number1 = Number(num1)
+    const number1 = Number(num1)
 	const number2 = Number(num2)
-
+    
 	const multiplyResult = number1 * number2
-
+    
 	const multiplyOperation = `${number1} * ${number2} = ${multiplyResult}`
-
+    
+    calc.addToHistory(multiplyOperation)
+    console.log(this.history);
 	console.log(multiplyResult)
 	return multiplyResult
 }
 
 //divide
 Calculator.prototype.divide = function (num1, num2) {
-	const number1 = Number(num1)
+    const number1 = Number(num1)
 	const number2 = Number(num2)
-
+    
 	const divideResult = number1 / number2
-
+    
 	const divideOperation = `${number1} / ${number2} = ${divideResult}`
-
+    
+    calc.addToHistory(divideOperation)
+    console.log(this.history);
 	console.log(divideResult)
 	return divideResult
 }
 
 //exponentiation
 Calculator.prototype.expo = function (num1, num2) {
-	const number1 = Number(num1)
+    const number1 = Number(num1)
 	const number2 = Number(num2)
-
+    
 	let iter = 0
 	let expoResult = 1
-	let info = ""
-
+	let expoOperation = ""
+    
 	while (iter < number2) {
-		expoResult = expoResult * number1
-
+        expoResult = expoResult * number1
+        
         if (iter > 0) {
-            info = info + " * "
+            expoOperation = expoOperation + " * "
         }
-		info = info + number1
+		expoOperation = expoOperation + number1
 		iter++
 	}
-	info = info + " = " + expoResult
-
+	expoOperation = expoOperation + " = " + expoResult
+    
+    calc.addToHistory(expoOperation)
+    console.log(this.history);
 	console.log(expoResult)
-	console.log(info)
+	console.log(expoOperation)
 	return expoResult
 }
+
+
+
+
 
 const calc = new Calculator()
 let action, promptContent, isCorrectAction, number1, number2
