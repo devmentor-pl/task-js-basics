4725627d78421046f5ffd7c3ffcbe4fd0efaafeb
Emilia Szafarska <emilia.anna.szafarska@gmail.com>
2024-04-02 19:15:15 +0200

    Add Calculator.prototype.add method

--- js/app.js
+++ js/app.js
@@ -13,9 +13,17 @@ Calculator.prototype.getHistoryAsString = function() {
 
 Calculator.prototype.add = function(num1, num2) {
     // 1. zamień wartości przekazane przez parametr na typ number
+    num1 = Number(num1);
+    num2 = Number(num2);
     // 2. sprawdź czy są one poprawne
-    // 3. jeśli tak to wykonaj działanie i zapisz jego resultat
-    // 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
+    if(!isNaN(num1) && !isNaN(num2)) {
+        // 3. jeśli tak to wykonaj działanie i zapisz jego rezultat
+        const result = num1 + num2;
+        // 4. dodaj do historii operacji to działanie w formie: 1 + 1 = 2
+        this.history.push(`${num1} + ${num2} = ${result}`)
+    } else { 
+        this.history.push('Niepoprawne dane!')
+    } 
 }
 
 const calc = new Calculator();
