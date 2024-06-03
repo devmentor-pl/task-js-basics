42d53c21cf7a126273d236b2792d268833b44ddc
ajek1993@gmail.com <ajek1993@gmail.com>
2024-06-03 23:49:22 +0200

    add methods

--- js/app.js
+++ js/app.js
@@ -1,39 +1,107 @@
 function Calculator() {
-    this.actions = ['+', '-', '*', '/', '^'];
-    this.history = [];
+  this.actions = ["+", "-", "*", "/", "^"];
+  this.history = [];
 }
 
-Calculator.prototype.isCorrectAction = function(action) {
-    return this.actions.includes(action);
-}
+Calculator.prototype.isCorrectAction = function (action) {
+  if (!this.actions.includes(action)) {
+    alert("Podano błędny operator");
+    return false;
+  }
+  return this.actions.includes(action);
+};
 
-Calculator.prototype.getHistoryAsString = function() {
-    return this.history.join('\n');
-}
+Calculator.prototype.getHistoryAsString = function () {
+  return this.history.join("\n");
+};
 
-Calculator.prototype.add = function(num1, num2) {
-    // 1. zamień wartości przekazane przez parametr na typ number
-    // 2. sprawdź czy są one poprawne
-    // 3. jeśli tak to wykonaj działanie i zapisz jego resultat
-    // 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
-}
+Calculator.prototype.verifyInputs = function (num1, num2) {
+  let error = "";
+  if (isNaN(+num1)) error += "Pierwszy argument nie jest liczbą \n";
+  if (isNaN(+num2)) error += "Drugi argument nie jest liczbą";
+
+  return error;
+};
+
+Calculator.prototype.add = function (num1, num2) {
+  if (this.verifyInputs(num1, num2)) {
+    return alert(this.verifyInputs(num1, num2));
+  }
+  this.history.push(`${+num1} + ${+num2} = ${+num1 + +num2}`);
+};
+
+Calculator.prototype.substract = function (num1, num2) {
+  if (this.verifyInputs(num1, num2)) {
+    return alert(this.verifyInputs(num1, num2));
+  }
+  this.history.push(`${+num1} - ${+num2} = ${+num1 - +num2}`);
+};
+
+Calculator.prototype.multiply = function (num1, num2) {
+  if (this.verifyInputs(num1, num2)) {
+    return alert(this.verifyInputs(num1, num2));
+  }
+  this.history.push(`${+num1} * ${+num2} = ${+num1 * +num2}`);
+};
+
+Calculator.prototype.divide = function (num1, num2) {
+  if (this.verifyInputs(num1, num2)) {
+    return alert(this.verifyInputs(num1, num2));
+  }
+  this.history.push(`${+num1} / ${+num2} = ${+num1 / +num2}`);
+};
+
+Calculator.prototype.power = function (num1, num2) {
+  if (this.verifyInputs(num1, num2)) {
+    return alert(this.verifyInputs(num1, num2));
+  }
+  let i = num2;
+  let result = 1;
+  while (i > 0) {
+    result *= num1;
+    i--;
+  }
+  if (num2 < 0) {
+    return alert("Potęga nie może być ujemna");
+  }
+  this.history.push(`${+num1} ^ ${+num2} = ${result}`);
+};
 
 const calc = new Calculator();
 let action, promptContent, isCorrectAction, number1, number2;
-do { 
-    promptContent = 'Podaj jaką operację chcesz wykonać (+, -, *, /, ^) i potwierdź. \n'; // \n - znak nowej linii
-    promptContent += 'Jeśli chcesz zrezygnować wciśnij Anuluj. \n';
-    promptContent += 'Lista poprzednich operacji: \n' + calc.getHistoryAsString();
+do {
+  promptContent =
+    "Podaj jaką operację chcesz wykonać (+, -, *, /, ^) i potwierdź. \n"; // \n - znak nowej linii
+  promptContent += "Jeśli chcesz zrezygnować wciśnij Anuluj. \n";
+  promptContent += "Lista poprzednich operacji: \n" + calc.getHistoryAsString();
 
-    action = prompt(promptContent);
-    isCorrectAction = calc.isCorrectAction(action);
-    if(isCorrectAction) {
-        number1 = prompt('Podaj liczbę nr 1');
-        number2 = prompt('Podaj liczbę nr 2');
+  action = prompt(promptContent);
+  isCorrectAction = calc.isCorrectAction(action);
+  if (isCorrectAction) {
+    number1 = prompt("Podaj liczbę nr 1");
+    number2 = prompt("Podaj liczbę nr 2");
 
-        if(action === '+') {
-            calc.add(number1, number2);
-        }
+    switch (action) {
+      case "+": {
+        calc.add(number1, number2);
+        break;
+      }
+      case "-": {
+        calc.substract(number1, number2);
+        break;
+      }
+      case "*": {
+        calc.multiply(number1, number2);
+        break;
+      }
+      case "/": {
+        calc.divide(number1, number2);
+        break;
+      }
+      case "^": {
+        calc.power(number1, number2);
+        break;
+      }
     }
-    
-} while(calc.isCorrectAction(action));
+  }
+} while (calc.isCorrectAction(action));
