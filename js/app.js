862a392767dca7dc9f35e3e6d2f308cc75428d4e
Dagmara Opalka <dagmara-opalka@wp.pl>
2024-08-19 22:28:37 +0200

    commit of task js basics

--- js/app.js
+++ js/app.js
@@ -13,10 +13,81 @@ Calculator.prototype.getHistoryAsString = function() {
 
 Calculator.prototype.add = function(num1, num2) {
     // 1. zamień wartości przekazane przez parametr na typ number
+    num1 = Number(num1);
+    num2 = Number(num2);
     // 2. sprawdź czy są one poprawne
+    if (isNaN(num1) || isNaN(num2)) {
+        alert('Podane wartości są niepoprawne');
+        return;
+    }
     // 3. jeśli tak to wykonaj działanie i zapisz jego resultat
+    const result = num1 + num2;
     // 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
+    this.history.push(`${num1} + ${num2} = ${result}`);
 }
+
+Calculator.prototype.subtract = function(num1, num2) {
+    num1 = Number(num1);
+    num2 = Number(num2);
+    
+    if (isNaN(num1) || isNaN(num2)) {
+        alert('Podano niepoprawne wartości. Spróbuj ponownie.');
+        return;
+    }
+
+    const result = num1 - num2;
+    this.history.push(`${num1} - ${num2} = ${result}`);
+};
+
+Calculator.prototype.multiply = function(num1, num2) {
+    num1 = Number(num1);
+    num2 = Number(num2);
+
+    if (isNaN(num1) || isNaN(num2)) {
+        alert('Podano niepoprawne wartości. Spróbuj ponownie.');
+        return;
+    }
+
+    const result = num1 * num2;
+    this.history.push(`${num1} * ${num2} = ${result}`);
+};
+
+Calculator.prototype.divide = function(num1, num2) {
+    num1 = Number(num1);
+    num2 = Number(num2);
+
+    if (isNaN(num1) || isNaN(num2)) {
+        alert('Podano niepoprawne wartości. Spróbuj ponownie.');
+        return;
+    }
+
+    if (num2 === 0) {
+        alert('Nie można dzielić przez 0. Spróbuj ponownie.');
+        return;
+    }
+
+    const result = num1 / num2;
+    this.history.push(`${num1} / ${num2} = ${result}`);
+};
+
+Calculator.prototype.power = function(num1, num2) {
+    num1 = Number(num1);
+    num2 = Number(num2);
+
+    if (isNaN(num1) || isNaN(num2)) {
+        alert('Podano niepoprawne wartości. Spróbuj ponownie.');
+        return;
+    }
+
+    let result = 1;
+    for (let i = 0; i < Math.abs(num2); i++) {
+        result *= num1;
+    }
+
+    this.history.push(`${num1} ^ ${num2} = ${result}`);
+};
+
+
 
 const calc = new Calculator();
 let action, promptContent, isCorrectAction, number1, number2;
@@ -31,8 +102,22 @@ do {
         number1 = prompt('Podaj liczbę nr 1');
         number2 = prompt('Podaj liczbę nr 2');
 
-        if(action === '+') {
-            calc.add(number1, number2);
+        switch (action) {
+            case '+':
+                calc.add(number1, number2);
+                break;
+            case '-':
+                calc.subtract(number1, number2);
+                break;
+            case '*':
+                calc.multiply(number1, number2);
+                break;
+            case '/':
+                calc.divide(number1, number2);
+                break;
+            case '^':
+                calc.power(number1, number2);
+                break;
         }
     }
     
